d2924cd270f7b21f340a4f452c28921c82606b9d
Andrew Mfune <andiemfune@gmail.com>
2022-01-26 08:34:04 +0200

    Added registration encounter

--- src/apps/ANC/app.ts
+++ src/apps/ANC/app.ts
@@ -2,6 +2,7 @@ import { AppInterface } from "../interfaces/AppInterface";
 import AncRoutes from "./Config/AncRoutes";
 import HomePageStats from "@/apps/ANC/Components/HomeStats.vue"
 import { PRIMARY_ACTIVITIES } from "./Config/AncProgramActivites";
+import { AppEncounterService } from "@/services/app_encounter_service";
 
 const ANC: AppInterface = {
     programID: 12,
@@ -12,6 +13,12 @@ const ANC: AppInterface = {
     primaryPatientActivites: PRIMARY_ACTIVITIES,
     secondaryPatientActivites: [],
     homeOverviewComponent: HomePageStats,
+    async onRegisterPatient(patientID: number) {
+        // Registration Encounter
+        const registration = new AppEncounterService(patientID, 5, -1)
+        await registration.createEncounter()
+        await registration.saveValueCodedObs('Type of patient', 'New patient')
+    },
     programPatientIdentifiers: {
         'National ID': {
             id: 28,
