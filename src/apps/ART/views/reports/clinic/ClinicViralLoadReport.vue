32d2feb74d03339bfc0e2107ff7eea2f64e50e7d
Andrew Mfune <andiemfune@gmail.com>
2022-02-04 19:18:39 +0200

    Now appending result modifier to results

--- src/apps/ART/views/reports/clinic/ClinicViralLoadReport.vue
+++ src/apps/ART/views/reports/clinic/ClinicViralLoadReport.vue
@@ -85,7 +85,6 @@ export default defineComponent({
             this.setRows((await this.report.getViralLoadResults(resultType.value.toLowerCase())))
         },
         async setRows(data: Array<any>) {
-            console.log(data)
             data.forEach((d: any) => {
                this.rows.push([
                     table.td(d.arv_number),
@@ -93,7 +92,7 @@ export default defineComponent({
                     table.tdDate(d.birthdate),
                     table.td(d.specimen),
                     table.tdDate(d.order_date),
-                    table.td(d.result),
+                    table.td(`${d.result_modifier || ''} ${d.result}`),
                     table.tdDate(d.result_date)                    
                ])
             })
