bfb38e9323453da56fd7069a739034432cf3804e
Andrew Mfune <andiemfune@gmail.com>
2022-02-04 17:18:18 +0200

    Added date interval selection for Arvs for transferred in patients

--- src/apps/ART/views/encounters/Registration.vue
+++ src/apps/ART/views/encounters/Registration.vue
@@ -419,6 +419,52 @@ export default defineComponent({
                     }
                 },
                 {
+                    id: 'drug_interval',
+                    helpText: 'Interval for last received ARVs',
+                    type: FieldType.TT_NEXT_VISIT_INTERVAL_SELECTION,
+                    condition: (f: any) => `${f.received_arvs.value}`.match(/yes/i),
+                    validation: (val: Option) => Validation.required(val),
+                    unload: (v: Option) => {
+                        this.prescription.setNextVisitInterval(v.value)
+                    },
+                    options: () => {
+                        const intervals = [
+                            { label: '2 weeks', value: 14 },
+                            { label: '1 month', value: 28 },
+                            { label: '2 months', value: 56 },
+                            { label: '3 months', value: 84 },
+                            { label: '4 months', value: 112 },
+                            { label: '5 months', value: 140 },
+                            { label: '6 months', value: 168 },
+                            { label: '7 months', value: 196 },
+                            { label: '8 months', value: 224 },
+                            { label: '9 months', value: 252 },
+                            { label: '10 months', value: 280 },
+                            { label: '11 months', value: 308 },                        
+                            { label: '12 months', value: 336 },
+                        ]
+                        return intervals.map(({label, value}: Option) => {
+                            this.prescription.setNextVisitInterval(value)
+                            const nextAppointment = this.prescription.calculateDateFromInterval()
+                            return {
+                                label,
+                                value,
+                                other: {
+                                    label: 'Medication run-out date:',
+                                    value: HisDate.toStandardHisDisplayFormat(nextAppointment),
+                                    other: {
+                                        label: "",
+                                        value: []
+                                    }
+                                }
+                            }
+                        })
+                    },
+                    config: {
+                        showRegimenCardTitle: false
+                    }
+                },
+                {
                     id: 'has_transfer_letter',
                     helpText: 'Has staging information?',
                     type: FieldType.TT_SELECT,
