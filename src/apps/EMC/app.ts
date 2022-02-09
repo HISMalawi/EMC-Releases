9907697886bf26c6540eae422e0d623675ca455f
Daniel Justin <danieljmuleke@gmail.com>
2022-02-09 09:08:32 +0200

    fix errors after merge

--- src/apps/EMC/app.ts
+++ src/apps/EMC/app.ts
@@ -1,31 +1,28 @@
 import { AppInterface } from "../interfaces/AppInterface";
-import { GlobalPropertyService } from "@/services/global_property_service"
+import GLOBAL_PROP from "@/apps/GLOBAL_APP/global_prop";
 import EmcRoutes from "./Config/EmcRoutes";
 
 const EMC: AppInterface = {
-    programID: 1,
-    isPocApp: false,
-    appLandingPage: '/emc/home',
-    applicationName: 'EMC',
-    applicationIcon: 'aids.png',
-    applicationDescription: "HIV Electronic Master card App",
-    appRoutes: EmcRoutes,
-    homeOverviewComponent: {},
-    primaryPatientActivites: [],
-    secondaryPatientActivites: [],
-    globalPropertySettings: [],
-    programReports: [],
-    programPatientIdentifiers: {
-        'ARV Number': {
-            id: 4,
-            name: 'ARV Number',
-            isPrimary: true,
-            useForSearch: true,
-            prefix: async () => {
-                const prefix = await GlobalPropertyService.getSitePrefix()
-                return `${prefix}-ARV-`
-            }
-        }
-    }
-}
-export default EMC
+  programID: 1,
+  isPocApp: false,
+  appLandingPage: "/emc/home",
+  applicationName: "EMC",
+  applicationIcon: "aids.png",
+  applicationDescription: "HIV Electronic Master card App",
+  appRoutes: EmcRoutes,
+  homeOverviewComponent: {},
+  primaryPatientActivites: [],
+  secondaryPatientActivites: [],
+  globalPropertySettings: [],
+  programReports: [],
+  programPatientIdentifiers: {
+    "ARV Number": {
+      id: 4,
+      name: "ARV Number",
+      isPrimary: true,
+      useForSearch: true,
+      prefix: async () => `${await GLOBAL_PROP.sitePrefix()}-ARV-`,
+    },
+  },
+};
+export default EMC;
