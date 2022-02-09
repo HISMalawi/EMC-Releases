8d80907002e4741127fced9cad330f69480eb390
Daniel Justin <danieljmuleke@gmail.com>
2022-02-09 09:35:04 +0200

    Added mastercard login routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,194 +1,200 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import Login from '../views/Login/Login.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import Login from "../views/Login/Login.vue";
 
-import Home from '../views/Home.vue';
-import Confirmation from '../views/Confirmation.vue';
-import HClocation from '../views/HClocation.vue';
+import Home from "../views/Home.vue";
+import Confirmation from "../views/Confirmation.vue";
+import HClocation from "../views/HClocation.vue";
 import SearchPatient from "@/views/SearchPatient.vue";
-import SearchClient from '../views/SearchClient.vue'
-import PatientRegistration from '../views/PatientRegistration.vue'
-import GuardianRegistration from '../views/GuardianRegistration.vue'
-import SearchResults from '../views/SearchResults.vue'
-import Example from '../views/Example.vue'
-import FindByID from '../views/FindByID.vue'
-import PatientDashboard from '../views/PatientDashboard.vue'
-import Configuration from '@/views/Configuration.vue'
-import HisApps from '@/apps/his_apps';
-import SessionDate from "@/views/SessionDate.vue"
-import SystemUsage from "@/views/SystemUsage.vue"
-import PrintLocation from "@/views/PrintLocation.vue"
-import PortalSettings from "@/views/PortalSettings.vue"
-import HostConfig from '@/views/HostConfig.vue'
-import ProgramManagement from "@/views/ProgramManagement.vue"
-import LabResults from "@/views/LabResults.vue"
-import User from "@/views/NewUser.vue"
-import PatientMerging from "@/views/PatientMerging.vue"
-import NpidDuplicates from "@/views/NpidDuplicates.vue"
+import SearchClient from "../views/SearchClient.vue";
+import PatientRegistration from "../views/PatientRegistration.vue";
+import GuardianRegistration from "../views/GuardianRegistration.vue";
+import SearchResults from "../views/SearchResults.vue";
+import Example from "../views/Example.vue";
+import FindByID from "../views/FindByID.vue";
+import PatientDashboard from "../views/PatientDashboard.vue";
+import Configuration from "@/views/Configuration.vue";
+import HisApps from "@/apps/his_apps";
+import SessionDate from "@/views/SessionDate.vue";
+import SystemUsage from "@/views/SystemUsage.vue";
+import PrintLocation from "@/views/PrintLocation.vue";
+import PortalSettings from "@/views/PortalSettings.vue";
+import HostConfig from "@/views/HostConfig.vue";
+import ProgramManagement from "@/views/ProgramManagement.vue";
+import LabResults from "@/views/LabResults.vue";
+import User from "@/views/NewUser.vue";
+import PatientMerging from "@/views/PatientMerging.vue";
+import NpidDuplicates from "@/views/NpidDuplicates.vue";
 
 const HIS_APP_ROUTES = (() => {
-  let routes: Array<RouteRecordRaw> = []
-  HisApps.forEach(app => {
+  let routes: Array<RouteRecordRaw> = [];
+  HisApps.forEach((app) => {
     if (app.appRoutes) {
-      routes = [...routes, ...app.appRoutes]
+      routes = [...routes, ...app.appRoutes];
     }
-  })
-  return routes
-})()
+  });
+  return routes;
+})();
 
 const routes: Array<RouteRecordRaw> = [
   ...HIS_APP_ROUTES,
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/npid/duplicates/:npid',
-    name: 'NPID Duplicates',
-    component: NpidDuplicates
+    path: "/npid/duplicates/:npid",
+    name: "NPID Duplicates",
+    component: NpidDuplicates,
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
+    path: "/home",
+    name: "Home",
+    component: Home,
   },
   {
-    name: 'View Duplicates',
-    path: '/view_duplicates',
-    component : () => import('@/views/DuplicateIdentifiers.vue')
+    name: "View Duplicates",
+    path: "/view_duplicates",
+    component: () => import("@/views/DuplicateIdentifiers.vue"),
   },
   {
-    path: '/example/desktop',
-    name: 'Desktop Example',
-    component: () => import('@/views/DTFormExample.vue')
+    path: "/example/desktop",
+    name: "Desktop Example",
+    component: () => import("@/views/DTFormExample.vue"),
   },
   {
-    name: 'Update site location',
-    path:'/location/update/site',
-    component: () => import('@/views/SiteLocation.vue')
+    name: "Update site location",
+    path: "/location/update/site",
+    component: () => import("@/views/SiteLocation.vue"),
   },
   {
-    name: 'Update site code',
-    path:'/location/update/code',
-    component: () => import('@/views/SetSiteCode.vue')
+    name: "Update site code",
+    path: "/location/update/code",
+    component: () => import("@/views/SetSiteCode.vue"),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/search_patient',
-    name: 'Search Patient',
-    component: SearchPatient
+    name: "EMC Login",
+    path: "/emc/login",
+    component: () => import("@/apps/EMC/views/Login.vue"),
   },
   {
-    path: '/session/date',
-    name: 'Session Date',
-    component: SessionDate
+    path: "/search_patient",
+    name: "Search Patient",
+    component: SearchPatient,
   },
   {
-    path: '/users/usage',
-    name: 'User system usage',
-    component: SystemUsage
+    path: "/session/date",
+    name: "Session Date",
+    component: SessionDate,
   },
   {
-    path: '/print/location',
-    name: 'Print Location',
-    component: PrintLocation
+    path: "/users/usage",
+    name: "User system usage",
+    component: SystemUsage,
   },
   {
-    path: '/select_hc_location',
-    name: 'HC Location',
-    component: HClocation
+    path: "/print/location",
+    name: "Print Location",
+    component: PrintLocation,
   },
   {
-    path: '/search_client',
-    name: 'Search client',
-    component: SearchClient
+    path: "/select_hc_location",
+    name: "HC Location",
+    component: HClocation,
   },
   {
-    path: '/patient/dashboard/:id',
-    name: 'Patient Dashboard',
-    component: PatientDashboard
+    path: "/search_client",
+    name: "Search client",
+    component: SearchClient,
   },
   {
-    path: '/patient/registration',
-    name: 'Patient Registration',
-    component: PatientRegistration
+    path: "/patient/dashboard/:id",
+    name: "Patient Dashboard",
+    component: PatientDashboard,
   },
   {
-    path: '/guardian/registration/:patient_id',
-    name: 'Guardian Registration',
-    component: GuardianRegistration
+    path: "/patient/registration",
+    name: "Patient Registration",
+    component: PatientRegistration,
   },
   {
-    path: '/example',
-    name: 'Example',
-    component: Example
+    path: "/guardian/registration/:patient_id",
+    name: "Guardian Registration",
+    component: GuardianRegistration,
   },
   {
-    path: '/patient/search_results',
-    name: 'Patient Search results',
-    component: SearchResults
+    path: "/example",
+    name: "Example",
+    component: Example,
   },
   {
-    path: '/patients/search/id',
-    name: 'ID Search',
-    component: FindByID
+    path: "/patient/search_results",
+    name: "Patient Search results",
+    component: SearchResults,
   },
   {
-    path: '/patients/confirm',
-    name: 'Patient confirmation',
-    component: Confirmation
+    path: "/patients/search/id",
+    name: "ID Search",
+    component: FindByID,
   },
   {
-    path: '/patients/merge',
-    name: 'Patient Merging',
-    component: PatientMerging
+    path: "/patients/confirm",
+    name: "Patient confirmation",
+    component: Confirmation,
   },
   {
-    path: '/patient/programs/:patient_id',
-    name: 'Program(s)',
-    component: ProgramManagement
+    path: "/patients/merge",
+    name: "Patient Merging",
+    component: PatientMerging,
   },
   {
-    path: '/preferences',
-    name: 'Preferences',
-    component: Configuration
+    path: "/patient/programs/:patient_id",
+    name: "Program(s)",
+    component: ProgramManagement,
   },
   {
-    path: '/user',
-    name: 'User',
-    component: User
+    path: "/preferences",
+    name: "Preferences",
+    component: Configuration,
   },
   {
-    path: '/settings/host',
-    name: 'API host settings',
-    component: HostConfig
+    path: "/user",
+    name: "User",
+    component: User,
   },
   {
-    path: '/lab/results/:patient_id',
-    name: 'Lab Results',
-    component: LabResults
+    path: "/settings/host",
+    name: "API host settings",
+    component: HostConfig,
   },
   {
-    path: '/portal/config',
-    name: 'Portal settings',
-    component: PortalSettings
-  }
-]
+    path: "/lab/results/:patient_id",
+    name: "Lab Results",
+    component: LabResults,
+  },
+  {
+    path: "/portal/config",
+    name: "Portal settings",
+    component: PortalSettings,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-router.beforeEach((to, from, next) => {
-  const whitelistedUri = ['/login', '/settings/host']
-  if (!sessionStorage.getItem('apiKey') && !whitelistedUri.includes(to.path)) {
-    next('/login')
+router.beforeEach((to, _from, next) => {
+  const whitelistedUri = ["/login", "/settings/host", "/emc/login"];
+  if (!sessionStorage.getItem("apiKey") && !whitelistedUri.includes(to.path)) {
+    const goTo = to.path.match(/emc/i) ? "/emc/login" : "/login";
+    next(goTo);
   }
-  next()
-})
-export default router
+  next();
+});
+export default router;
