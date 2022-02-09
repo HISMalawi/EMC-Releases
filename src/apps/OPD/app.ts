6edc67dd1db7069705f2eb4eb2e2693c8e13feac
Daniel Justin <danieljmuleke@gmail.com>
2022-02-09 09:17:36 +0200

    fixes after merge

--- src/apps/OPD/app.ts
+++ src/apps/OPD/app.ts
@@ -1,44 +1,49 @@
-import { AppInterface, GeneralDataInterface } from '@/apps/interfaces/AppInterface';
+import {
+  AppInterface,
+  GeneralDataInterface,
+} from "@/apps/interfaces/AppInterface";
 import HomeOverview from "@/apps/OPD/components/HomeOverview.vue";
-import { PRIMARY_ACTIVITIES } from '@/apps/OPD/config/programActivities';
-import { REPORTS } from '@/apps/OPD/config/programReports';
-import opdRoutes  from '@/apps/OPD/config/routes';
-import { PatientProgramService } from '@/services/patient_program_service';
-import { AppEncounterService } from "@/services/app_encounter_service"
-import PatientAlerts from "@/services/patient_alerts"
-import { Observation } from '@/interfaces/observation';
-import { OrderService } from '@/services/order_service';
-import { RelationshipService } from '@/services/relationship_service';
-import { Order } from '@/interfaces/order';
+import { PRIMARY_ACTIVITIES } from "@/apps/OPD/config/programActivities";
+import { REPORTS } from "@/apps/OPD/config/programReports";
+import opdRoutes from "@/apps/OPD/config/routes";
+import { PatientProgramService } from "@/services/patient_program_service";
+import { AppEncounterService } from "@/services/app_encounter_service";
+import PatientAlerts from "@/services/patient_alerts";
+import { Observation } from "@/interfaces/observation";
+import { OrderService } from "@/services/order_service";
+import { RelationshipService } from "@/services/relationship_service";
+import { Order } from "@/interfaces/order";
 
-async function onRegisterPatient(patientId: number){
-  const program = new PatientProgramService(patientId)
-  await program.enrollProgram()
+async function onRegisterPatient(patientId: number) {
+  const program = new PatientProgramService(patientId);
+  await program.enrollProgram();
 
   // Create registration encounter
-  const encounter = new AppEncounterService(patientId, 5)
+  const encounter = new AppEncounterService(patientId, 5);
 
-  await encounter.createEncounter()
-  await encounter.saveValueCodedObs(
-    'Type of patient', 'New Patient'
-  )
+  await encounter.createEncounter();
+  await encounter.saveValueCodedObs("Type of patient", "New Patient");
 }
 
 async function formatPatientProgramSummary(data: any) {
-  // TODO: refactor core ProgramService to allow programs specify end-points for 
+  // TODO: refactor core ProgramService to allow programs specify end-points for
   // getting program information summary
-  const nationalID = (data && data.national_id) ? data.national_id : ''
-  const hivStatus = (data && data.hiv_status) ? data.hiv_status : ''
-  
+  const nationalID = data && data.national_id ? data.national_id : "";
+  const hivStatus = data && data.hiv_status ? data.hiv_status : "";
+
   return [
-    { label: 'Malawi National ID', value: nationalID},
-    { label: 'HIV Status', value: hivStatus }
-  ]
+    { label: "Malawi National ID", value: nationalID },
+    { label: "HIV Status", value: hivStatus },
+  ];
 }
 
-async function getPatientDashboardAlerts(patient: any): Promise<GeneralDataInterface[]>{
-  const sideEffects: Observation[] = await PatientAlerts.alertSideEffects(patient.getID())
-  const bmi = await patient.getBMI()
+async function getPatientDashboardAlerts(
+  patient: any
+): Promise<GeneralDataInterface[]> {
+  const sideEffects: Observation[] = await PatientAlerts.alertSideEffects(
+    patient.getID()
+  );
+  const bmi = await patient.getBMI();
   return [
     {
       label: "Side effects",
@@ -46,69 +51,68 @@ async function getPatientDashboardAlerts(patient: any): Promise<GeneralDataInter
     },
     {
       label: "Patient BMI is",
-      value: `${bmi.result}`
-    }
-  ]
+      value: `${bmi.result}`,
+    },
+  ];
 }
 
 function orderToString(order: Order) {
   const test = order.tests[0];
   const result = test.result[0];
-  const status = OrderService.isHighViralLoadResult(result) ? '(<b style="color: #eb445a;">High</b>)' : ''
+  const status = OrderService.isHighViralLoadResult(result)
+    ? '(<b style="color: #eb445a;">High</b>)'
+    : "";
   return `${test.name} ${result.value_modifier}${result.value} ${status}`;
 }
 
 function confirmationSummary(patient: any, program: any) {
   return {
-    'PATIENT IDENTIFIERS': () => ([
+    "PATIENT IDENTIFIERS": () => [
       {
         label: "NPID",
         value: patient.getNationalID(),
-      }
-    ]),
-    'ALERTS': () => getPatientDashboardAlerts(patient),
-    'LAB ORDERS': async () => {
-      const data: any = []
-      await OrderService.getOrders(patient.getID())
-        .then((orders) => {
-          const VLOrders = OrderService.getViralLoadOrders(orders);
-          VLOrders.forEach((order) => {
-            data.push({
-              value: orderToString(order),
-              label: ``,
-            });
+      },
+    ],
+    ALERTS: () => getPatientDashboardAlerts(patient),
+    "LAB ORDERS": async () => {
+      const data: any = [];
+      await OrderService.getOrders(patient.getID()).then((orders) => {
+        const VLOrders = OrderService.getViralLoadOrders(orders);
+        VLOrders.forEach((order) => {
+          data.push({
+            value: orderToString(order),
+            label: ``,
           });
         });
-      return data
+      });
+      return data;
     },
-    'OUTCOME': () => ([
-      { 
-        label: 'Current Outcome', 
-        value: program.outcome || 'N/A'
-      }
-    ]),
-    'GUARDIAN': async () => {
-      const req = await RelationshipService
-        .getGuardianDetails(
-            patient.getID()
-        )
+    OUTCOME: () => [
+      {
+        label: "Current Outcome",
+        value: program.outcome || "N/A",
+      },
+    ],
+    GUARDIAN: async () => {
+      const req = await RelationshipService.getGuardianDetails(patient.getID());
       if (req) {
         return req.map((r: any) => ({
           label: r.name,
           value: r.relationshipType,
-        }))
-      } 
-      return []
-    }
-  }
+        }));
+      }
+      return [];
+    },
+  };
 }
 
 const OPD: AppInterface = {
   programID: 14,
-  applicationName: 'OPD',
-  applicationIcon: 'opd.png',
-  applicationDescription: 'Outpatient Program',
+  applicationName: "OPD",
+  applicationIcon: "opd.png",
+  applicationDescription: "Outpatient Program",
   appRoutes: opdRoutes,
+  isPocApp: true,
   programReports: REPORTS,
   primaryPatientActivites: PRIMARY_ACTIVITIES,
   secondaryPatientActivites: [],
@@ -117,14 +121,14 @@ const OPD: AppInterface = {
   formatPatientProgramSummary,
   confirmationSummary,
   programPatientIdentifiers: {
-    'National ID': {
+    "National ID": {
       id: 28,
-      name: 'National ID',
+      name: "National ID",
       isPrimary: true,
       useForSearch: true,
-      prefix: () => ''
+      prefix: () => "",
     },
-  }
-}
+  },
+};
 
-export default OPD
+export default OPD;
