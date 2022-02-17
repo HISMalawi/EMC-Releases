61f4dde8664debc2e41b684f5dcd46152d8250c0
Daniel Justin <danieljmuleke@gmail.com>
2022-02-17 14:42:57 +0200

    Improved Appointment screen user interface

--- src/components/FormElements/HisAppointments.vue
+++ src/components/FormElements/HisAppointments.vue
@@ -241,8 +241,10 @@ export default defineComponent({
 .custom-calendar.vc-container .vc-day-dots {
     margin-bottom: 5px;
   }
-.custom-calendar.vc-container .vc-highlight {
+.custom-calendar.vc-container .vc-highlight,
+.custom-calendar.vc-container .vc-highlights .vc-day-layer {
   border-radius: 0 !important;
+  padding: 0 !important;
 }
 .selected{
   font-size: 3vh;
