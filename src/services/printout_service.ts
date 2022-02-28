b138138f0a75ac9d2384cad0197366b8d65672d9
Andrew Mfune <andiemfune@gmail.com>
2022-02-28 09:15:56 +0200

    Fix Hanging zebra printing modal screen.
    Hanging is mostly caused if multiple zebra modals try to appear at the same time

--- src/services/printout_service.ts
+++ src/services/printout_service.ts
@@ -6,17 +6,34 @@ import ApiClient from "./api_client";
 import usePlatform from "@/composables/usePlatform";
 import { toastWarning } from "@/utils/Alerts";
 
+enum PrintOutVariable {
+    ZEBRA_MODAL = 'zebra-modal'
+}
+
 export class PrintoutService extends Service {
     constructor() {
         super()
     }
 
+    static async zebraModalActive() {
+        const modal = await modalController.getTop()
+        return modal && modal.id === PrintOutVariable.ZEBRA_MODAL
+    }
+
     static async showPrinterImage() {
-        const modal = await modalController.create({
-            component: ZebraPrinterComponent,
-            backdropDismiss: false
-        })
-        modal.present()
+        /** 
+         * Prevent showing multiple modals when printing.
+         * From experience when multiple modals appear, the modal becomes
+         * undismissable
+        */
+        if (!(await this.zebraModalActive())) {
+            const modal = await modalController.create({
+                id: PrintOutVariable.ZEBRA_MODAL,
+                component: ZebraPrinterComponent,
+                backdropDismiss: false
+            })
+            modal.present()
+        }
     }
 
     async printLbl(url: any) {
@@ -27,21 +44,26 @@ export class PrintoutService extends Service {
                 // Do a preflight to make sure that we can print that label
                 // before changing document location
                 const preFetch = await Service.getText(url)
-                if (!preFetch) throw 'Unable to print Label. Try again later'
+                if (!preFetch) {
+                    throw 'Unable to print Label. Try again later'
+                } 
                 document.location = (await ApiClient.expandPath(url)) as any
-                await delayPromise(1500)
+                await delayPromise(2500)
             } catch (e) {
                 toastWarning(e, 3000)
+            }
+            if ((await PrintoutService.zebraModalActive())) {
+                modalController.dismiss({}, undefined, PrintOutVariable.ZEBRA_MODAL)
             }
         } else {
             toastWarning('Sorry, your Platform does not support Label printing. Bye!')
         }
-        return modalController.dismiss({})
     }
 
     async printLocation(locationId: number) {
         await this.printLbl(`labels/location?location_id=${locationId}`)
     }
+
     async printDrug(drugId: number, quantity: number) {
         await this.printLbl(`drugs/${drugId}/barcode?quantity=${quantity}`)
     }
