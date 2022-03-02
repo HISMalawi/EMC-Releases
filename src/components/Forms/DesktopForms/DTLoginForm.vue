7459635969b1f4d7cdbdd10f31683734fcef6f1a
Daniel Justin <danieljmuleke@gmail.com>
2022-03-02 23:02:29 +0200

    Implemented desktop login form

---
+++ src/components/Forms/DesktopForms/DTLoginForm.vue
@@ -0,0 +1,113 @@
+<template>
+  <ion-content fullscreen="false">
+    <ion-grid style="margin: 0; padding: 0">
+    <ion-row>
+      <ion-col size="8">
+        <ion-card-title
+          style="
+            text-align: center;
+            font-size: 78px;
+            font-weight: bold;
+            margin: 8vh 1px 0;
+          "
+        >
+          HIS APPS
+        </ion-card-title>
+        <ion-card-subtitle
+          style="text-align: center; font-size: 24px; margin-bottom: 20px"
+        >
+          version 1.2.1
+        </ion-card-subtitle>
+        <img
+          src="/assets/images/report.png"
+          style="display: block; width: 55%; margin: 5px auto"
+        />
+        <login-footer :showConfigBtn="showConfigBtn"></login-footer>
+      </ion-col>
+      <ion-col
+        size="4"
+        style="
+          background: #2839ad;
+          color: #ffff;
+          text-align: center;
+          height: 100vh;
+          padding: 38px 18px;
+        "
+      >
+        <ion-icon :icon="avatar" style="font-size: 8vw; margin: 10vh 1px 0" />
+        <h2>Welcome</h2>
+        <h4>Please, log in to your account</h4>
+        <ion-item style="margin: 2rem 3.5rem">
+          <ion-input
+            type="text"
+            placeholder="Username"
+            v-model="username"
+            :min="5"
+            required
+          />
+        </ion-item>
+        <ion-item style="margin: 2rem 3.5rem">
+          <ion-input
+            type="password"
+            v-model="password"
+            placeholder="Password"
+            :max="100"
+            :min="5"
+            required
+          />
+        </ion-item>
+        <ion-button
+          expand="full"
+          size="large"
+          style="margin: 2rem 3.5rem; text-transform: none"
+          @click.prevent="login"
+        >
+          Login
+        </ion-button>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+  </ion-content>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { IonButton, IonCardSubtitle, IonCardTitle, IonCol, IonGrid, IonInput, IonItem, IonRow } from "@ionic/vue";
+import { person } from "ionicons/icons";
+import LoginFooter from "@/components/LoginFooter.vue";
+
+export default defineComponent({
+  name: "DTLoginForm",
+  components: {
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonButton,
+    IonInput,
+    IonItem,
+    IonCardTitle,
+    IonCardSubtitle,
+    LoginFooter
+  },
+  props: {
+    showConfigBtn: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ["login"],
+  setup(_, { emit }) {
+    const username = ref('')
+    const password = ref('')
+    
+    const login = () => emit("login", {username: username.value, password: password.value})
+
+    return {
+      avatar: person,
+      username,
+      password,
+      login
+    }
+  }
+})
+</script>
--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,139 +1,113 @@
 <template>
   <ion-page>
-    <ion-header> 
-      <ion-toolbar>
-        <ion-title> 
-          <ion-label :style="{fontWeight: 'bolder', fontSize: '1.9em'}"> 
-            <b style="color: #8b4513">National </b>
-            <b style="color: #cd853f">EMR</b>
+    <template v-if="useVirtualInput">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>
+            <ion-label :style="{ fontWeight: 'bolder', fontSize: '1.9em' }">
+              <b style="color: #8b4513">National </b>
+              <b style="color: #cd853f">EMR</b>
+            </ion-label>
+          </ion-title>
+          <ion-label class="ion-padding" slot="end">
+            Version: <b>{{ version }}</b>
           </ion-label>
-        </ion-title>
-        <ion-label class="ion-padding" slot="end">
-          Version: <b>{{ version }}</b>
-        </ion-label>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content fullscreen="false">
-      <inputs :useVirtualInput="useVirtualInput"/>
-    </ion-content>
-    <ion-footer>
-      <ion-toolbar>
-        <span>
-          <img
-            id="coat"
-            :src="coatImg"
-            alt="Malawi Coat of Arms logo"
-          />
-          <img
-            id="pepfar"
-            :src="pepfarImg"
-            alt="PEPFAR logo"
-          />
-        </span>
-        <select v-model="platform" slot="end" class="devices">
-          <option
-            v-for="(device, index) in devices" 
-            :key="index"
-            :value="device.value"
-            :selected="device.value === platform"
-          >
-            {{ device.label }}
-          </option>
-        </select>
-        <ion-button
-          slot="end"
-          class="config"
-          router-link="/settings/host"
-          v-if="showConfig">
-          Configuration
-        </ion-button>
-      </ion-toolbar>
-    </ion-footer>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content fullscreen="false">
+        <inputs :useVirtualInput="useVirtualInput" />
+      </ion-content>
+      <LoginFooter
+        :showConfigBtn="showConfig"
+      ></LoginFooter>
+    </template>
+    <template v-else>
+      <DTLoginForm
+        @login="doLogin"
+        :showConfigBtn="showConfig"
+      ></DTLoginForm>
+    </template>
   </ion-page>
 </template>
 <script lang="ts">
-import Inputs from "./LoginCustomPage.vue";
-import { 
-  IonButton, 
-  IonPage, 
-  IonContent, 
-  IonFooter,
+import Inputs from "./TSLoginForm.vue";
+import {
+  IonPage,
+  IonContent,
   IonHeader,
   IonToolbar,
   IonTitle,
-  IonLabel
+  IonLabel,
 } from "@ionic/vue";
-import img from '@/utils/Img';
-import { onMounted, ref } from '@vue/runtime-core';
-import { AuthService } from '@/services/auth_service';
-import usePlatform from '@/composables/usePlatform';
-import { computed, watch } from 'vue';
+import { onMounted, ref } from "@vue/runtime-core";
+import { AuthService, InvalidCredentialsError } from "@/services/auth_service";
+import usePlatform from "@/composables/usePlatform";
+import { defineComponent } from "vue";
+import DTLoginForm from "@/components/Forms/DesktopForms/DTLoginForm.vue";
+import HisApp from "@/apps/app_lib";
+import { useRouter } from "vue-router";
+import { toastDanger, toastWarning } from "@/utils/Alerts";
+import LoginFooter from "@/components/LoginFooter.vue";
 
-export default {
+export default defineComponent({
   name: "login",
   components: {
     Inputs,
-    IonButton,
     IonTitle,
     IonLabel,
     IonPage,
     IonHeader,
     IonToolbar,
     IonContent,
-    IonFooter,
+    DTLoginForm,
+    LoginFooter,
   },
   setup() {
-    const { platformType, setPlatformType } = usePlatform()
-    const useVirtualInput = computed(() => platformType.value === 'mobile')
-    const platform = ref(platformType.value || 'Platform')
-    const version = ref('')
-    const devices = ref([
-      {label: 'Mobile', value: 'mobile'},
-      {label: "Desktop", value: "desktop"}
-    ])
+    const router = useRouter();
+    const auth = new AuthService();
+    const { useVirtualInput } = usePlatform();
+    const version = ref("");
 
-    watch(platform, p => {
-      if (['mobile', 'desktop'].includes(p)) setPlatformType(p as 'mobile' | 'desktop')  
-    })
+    const doLogin = async (user: any) => {
+      if (user.username && user.password) {
+        auth.setUsername(user.username);
+        try {
+          await auth.login(user.password);
+          auth.startSession();
+          const app = await HisApp.selectApplication();
+          if (app) {
+            app.isPocApp
+              ? router.push("/select_hc_location")
+              : app.appLandingPage
+              ? router.push(app.appLandingPage)
+              : router.push("/");
+          }
+        } catch (e) {
+          if (e instanceof InvalidCredentialsError) {
+            toastWarning("Invalid username or password");
+          } else {
+            toastDanger(e);
+          }
+        }
+      } else {
+        toastWarning("Complete form to log in");
+      }
+    };
 
     onMounted(async () => {
-      const auth = new AuthService()
-      const appV = await auth.getHeadVersion()
-      auth.setActiveVersion(appV)
-      const apiV = await auth.getApiVersion()
-      version.value = `${appV} / ${apiV}`
-    })
+      const auth = new AuthService();
+      const appV = await auth.getHeadVersion();
+      auth.setActiveVersion(appV);
+      const apiV = await auth.getApiVersion();
+      version.value = `${appV} / ${apiV}`;
+    });
+
     return {
-      devices,
       version,
-      platform,
       useVirtualInput,
-      coatImg: img('login-logos/Malawi-Coat_of_arms_of_arms.png'),
-      pepfarImg: img('login-logos/PEPFAR.png'),
-      showConfig: localStorage.getItem("useLocalStorage") === "true"
-    }
-  }
-}
+      showConfig: localStorage.getItem("useLocalStorage") === "true",
+      doLogin,
+    };
+  },
+});
 </script>
-<style scoped>
-#coat {
-  width: 8vw;
-  height: 8vw;
-}
-#pepfar {
-  width: 6vw;
-  height: 6vw;
-}
-.devices {
-  background-color: var(--ion-color-primary);
-  color: #fff;
-  border: 1px solid lightblue;
-  font-size: 1.3em;
-  padding: 1rem;
-  margin-right: .5rem;
-}
-.devices>option {
-  background: #fff;
-  color: black;
-}
-</style>
