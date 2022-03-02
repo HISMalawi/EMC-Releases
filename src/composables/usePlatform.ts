a0edca13c507038bfcb22aaf656041d789c20685
Daniel Justin <danieljmuleke@gmail.com>
2022-03-02 22:40:45 +0200

    Refactor to use singleton state pattern

--- src/composables/usePlatform.ts
+++ src/composables/usePlatform.ts
@@ -1,5 +1,5 @@
 import { getPlatforms } from "@ionic/vue";
-import { computed, ref, watch } from "vue";
+import { computed, nextTick, readonly, ref, watch } from "vue";
 
 export enum PLATFORM_SESSION_KEY {
   ACTIVE_PLATFORM = 'cur_platform'
@@ -19,24 +19,26 @@ export const DESKTOP_PLATFORMS = [
   'electron'
 ]
 
-export default function usePlatform () {
-  const isPlatform = (platformList: string[]) => getPlatforms().map(p => platformList.includes(p)).some(Boolean)
+const isPlatform = (platformList: string[]) => getPlatforms().map(p => platformList.includes(p)).some(Boolean)
 
-  const isMobile = () => isPlatform(MOBILE_PLATFORMS) || !isPlatform(DESKTOP_PLATFORMS)
+const isMobile = () => isPlatform(MOBILE_PLATFORMS) || !isPlatform(DESKTOP_PLATFORMS)
 
-  const isDesktop = () => isPlatform(DESKTOP_PLATFORMS) || !isMobile()
+const isDesktop = () => isPlatform(DESKTOP_PLATFORMS) || !isMobile()
 
-  const configuredPlatform = localStorage.getItem(PLATFORM_SESSION_KEY.ACTIVE_PLATFORM)
+const configuredPlatform = localStorage.getItem(PLATFORM_SESSION_KEY.ACTIVE_PLATFORM)
 
-  const platformType = ref(configuredPlatform!=null ? configuredPlatform : isDesktop() ? 'desktop' : 'mobile')
+const platformType = ref(configuredPlatform!=null ? configuredPlatform : isDesktop() ? 'desktop' : 'mobile')
 
-  const useVirtualInput = computed(() => platformType.value === 'mobile')
+const useVirtualInput = computed(() => platformType.value === 'mobile')
 
-  const setPlatformType = (platform: 'mobile' | 'desktop') => {
-    platformType.value = platform
+const setPlatformType = (platform: 'mobile' | 'desktop') => {
+  nextTick(() => {
     localStorage.setItem(PLATFORM_SESSION_KEY.ACTIVE_PLATFORM, platform)
-  }  
+    platformType.value = platform
+  })
+}
 
+export default function usePlatform () {
   return {
     platformType,
     useVirtualInput,
