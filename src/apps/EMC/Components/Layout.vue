44c9725e5ec57e6b00358da37608e0c7fc711740
Daniel Justin <danieljmuleke@gmail.com>
2022-03-04 09:48:09 +0200

    Removed tooltips

--- src/apps/EMC/Components/Layout.vue
+++ src/apps/EMC/Components/Layout.vue
@@ -28,10 +28,10 @@
       <ion-toolbar>
         <ion-buttons slot="start">
           <ion-menu-button auto-hide="false"> </ion-menu-button>
-          <ion-button @click="selectApp" v-tooltip title="select application">
+          <ion-button @click="selectApp" >
             <ion-icon :icon="apps"></ion-icon>
           </ion-button>
-          <ion-button @click="showAuthUserMenu" v-tooltip title="show user menu">
+          <ion-button @click="showAuthUserMenu" >
             <ion-icon :icon="person"></ion-icon>
           </ion-button>
         </ion-buttons>
@@ -67,7 +67,6 @@ import { appPages } from "../Config/appPages";
 import { apps, person } from "ionicons/icons";
 import { useRouter } from "vue-router";
 import AuthUserMenuVue from "./AuthUserMenu.vue";
-import { tooltip } from '@ionited/tooltip-vue';
 
 export default defineComponent({
   name: "App",
@@ -77,7 +76,6 @@ export default defineComponent({
       default: 'Dashboard' 
     }
   },
-  directives: { tooltip },
   components: {
     IonPage,
     IonContent,
