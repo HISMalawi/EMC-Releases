8e96bbfb3f3d67c474da117d13140cfba2e035bd
Daniel Justin <danieljmuleke@gmail.com>
2022-03-07 17:27:49 +0200

    Added missed appointments card

--- src/apps/EMC/services/dashboard.service.ts
+++ src/apps/EMC/services/dashboard.service.ts
@@ -9,4 +9,13 @@ export default class DashboardService {
       'end_date': endDate
     })
   }
+
+  static getMissedAppointments (today: string, range: Record<string, string>) {
+    return Service.getJsonSWR('missed_appointments', {
+      'date': today,
+      'start_date': range.start,
+      'end_date': range.end,
+      'program_id': 1
+    })
+  }
 }
--- src/apps/EMC/views/Dashboard.vue
+++ src/apps/EMC/views/Dashboard.vue
@@ -3,6 +3,11 @@
     <h1>Dashboard</h1>
     <ion-grid>
       <ion-row>
+        <ion-col size="3">
+          <dashboard-card :value="totalMissedAppointments" label="Missed Appointments" color="primary" :icon="calendar" />
+        </ion-col>
+      </ion-row>
+      <ion-row>
         <ion-col size="8">
           <visit-stats-chart 
             :startDate="range.start" 
@@ -14,23 +19,32 @@
   </layout>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import Layout from "@/apps/EMC/Components/Layout.vue";
 import VisitStatsChart from "../Components/VisitStatsChart.vue";
 import { IonGrid, IonRow, IonCol } from "@ionic/vue";
 import DashboardCard from "../Components/DashboardCard.vue";
 import { people, calendar, alarm } from "ionicons/icons";
 import HisDate from "@/utils/Date";
+import DashboardService from "../services/dashboard.service";
 
 export default defineComponent({
   components: { Layout, VisitStatsChart, IonGrid, IonRow, IonCol, DashboardCard },
   setup() {
     const range = HisDate.getDateQuarter(new Date())
+    const today = HisDate.toStandardHisFormat(new Date())
+    const missedAppointments = DashboardService.getMissedAppointments(today, range)
+    const totalMissedAppointments = computed(() => missedAppointments.value 
+      ? missedAppointments.value.length
+      : -1
+    )
+    
     return {
       people,
       calendar,
       alarm,
       range,
+      totalMissedAppointments
     }
   }
 });
