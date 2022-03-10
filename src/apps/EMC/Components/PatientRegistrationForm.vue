e3f4796c0406f2fbdfc019c5d9322e3f692e8b84
Daniel Justin <danieljmuleke@gmail.com>
2022-03-10 22:45:26 +0200

    Refactor registration to a separate component

---
+++ src/apps/EMC/Components/PatientRegistrationForm.vue
@@ -0,0 +1,226 @@
+<template>
+  <ion-grid>
+    <ion-row class="his-card">
+        <ion-col size="12">
+          <ion-title class=" ion-text-center ion-margin-vertical"><b>Personal Details</b></ion-title>
+          <ion-row class="ion-margin-bottom">
+            <ion-col size="4">
+              <ion-label style="margin-bottom: 1em">First Name (*): </ion-label>
+              <ion-input v-model="patient.givenName.value" placeholder="First Name" class="box ion-margin-top" />
+            </ion-col>
+            <ion-col size="4">
+              <ion-label class="ion-padding-end">Middle Name: </ion-label>
+              <ion-input v-model="patient.middleName.value" class="box ion-margin-top" placeholder="Middle Name" />
+            </ion-col>
+            <ion-col size="4">
+              <ion-label class="ion-padding-end">Family Name (*): </ion-label>
+              <ion-input  v-model="patient.familyName.value" class="box ion-margin-top" placeholder="Family Name" />
+            </ion-col>
+          </ion-row>
+          <ion-row class="ion-margin-bottom">
+            <ion-col size="7">
+              <ion-row>
+                <ion-col size="12">
+                  <ion-label>
+                    <span v-if="estimateAge">Estimage Age (*):</span>
+                    <span v-else>Date of Birth (*):</span>
+                    <span class="ion-float-right ion-margin-end checkbox-label">
+                      Estimate Date of Birth
+                      <ion-checkbox v-model="estimateAge"></ion-checkbox>
+                    </span>
+                  </ion-label>
+                </ion-col>
+              </ion-row>
+              <ion-row>
+                <template v-if="estimateAge">
+                  <ion-col size="12">
+                    <ion-input v-model="patient.birthdate.age" class="box" :min="1" type="number" placeholder="Enter age estimate" />
+                  </ion-col>
+                </template>
+                <template v-else>
+                  <ion-col size="4">
+                    <ion-input v-model="patient.birthdate.day" class="box" :min="1" type="number" placeholder="DD" />
+                  </ion-col>
+                  <ion-col size="4">
+                    <ion-input v-model="patient.birthdate.month" class="box" :min="1" type="number" placeholder="MM" />
+                  </ion-col>
+                  <ion-col size="4">
+                    <ion-input v-model="patient.birthdate.year" class="box" :min="1920" type="number" placeholder="YYYY" />
+                  </ion-col>
+                </template>
+              </ion-row>
+            </ion-col>
+            <ion-col size="5" style="padding-top: .4rem;">
+              <ion-label >Gender (*): </ion-label>
+              <ion-select class="box ion-margin-top" v-model="patient.gender.value">
+                <ion-select-option>Male</ion-select-option>
+                <ion-select-option>Female</ion-select-option>
+              </ion-select>
+            </ion-col>
+          </ion-row>
+          <ion-row class="ion-margin-bottom">
+            <ion-col size="12">
+              <ion-label>
+                Cellphone Number (*):
+                <span class="ion-float-right ion-margin-end checkbox-label">
+                  Cellphone Number Unknown? 
+                  <ion-checkbox v-model="patientPhoneUnknown"></ion-checkbox>
+                </span>
+              </ion-label>
+              <ion-input v-model="patient.cellphone.value" :disabled="patient.cellphone.disabled" placeholder="Cellphone Number" class="box ion-margin-top" />
+            </ion-col>
+          </ion-row>
+          <ion-title class=" ion-text-center ion-margin-vertical"><b>Physical Address</b></ion-title>
+          <ion-row class="ion-margin-bottom">
+            <ion-col size="6">
+              <ion-label>
+                Home Village (*):
+                <span class="ion-float-right ion-margin-end checkbox-label">
+                  Village not listed? 
+                  <ion-checkbox v-model="addCustomVillage"></ion-checkbox>
+                </span>
+              </ion-label>
+              <ion-input v-if="addCustomVillage" v-model="patient.homeVillage.value" placeholder="enter village" class="box ion-margin-top" />
+              <ion-select class="box ion-margin-top" placeholder="Select Village" v-model="patient.homeVillage.value" v-else>
+                <ion-select-option>Test Village</ion-select-option>
+                <ion-select-option>Test Village</ion-select-option>
+              </ion-select>
+            </ion-col>
+            <ion-col size="6">
+              <ion-label>
+                Closest Landmark or Plot Number (*):
+                <span class="ion-float-right ion-margin-end checkbox-label">
+                  Landark not listed? 
+                  <ion-checkbox v-model="addCustomLandmark"></ion-checkbox>
+                </span> 
+              </ion-label>
+              <ion-input v-if="addCustomLandmark" v-model="patient.landmark.value" placeholder="enter closest landmark or plot number" class="box ion-margin-top" />
+              <ion-select class="box ion-margin-top" placeholder="Select Landmark" v-model="patient.landmark.value" v-else>
+                <ion-select-option>Church</ion-select-option>
+                <ion-select-option>Primary School</ion-select-option>
+              </ion-select>
+            </ion-col>
+          </ion-row>
+          <ion-title class=" ion-text-center ion-margin-vertical">
+            <b>Guardian details</b>
+            <span class="ion-margin-start checkbox-label">
+              Guardian Details Unknown? 
+              <ion-checkbox v-model="guardianNotAvailable"></ion-checkbox>
+            </span>
+          </ion-title>
+          <ion-row class="ion-margin-bottom">
+            <ion-col size="4">
+              <ion-label>First Name (*): </ion-label>
+              <ion-input v-model="guardian.givenName.value" :disabled="guardian.givenName.disabled" placeholder="First Name" class="box ion-margin-top" />
+            </ion-col>
+            <ion-col size="4">
+              <ion-label>Family Name (*): </ion-label>
+              <ion-input v-model="guardian.familyName.value" :disabled="guardian.familyName.disabled" class="box ion-margin-top" placeholder="Family Name" />
+            </ion-col>
+            <ion-col size="4">
+              <ion-label>
+                Cellphone Number (*):
+                <span class="ion-float-right ion-margin-end checkbox-label">
+                  Cellphone Number Unknown? 
+                  <ion-checkbox v-model="guardianPhoneUnknown"></ion-checkbox>
+                </span>
+              </ion-label>
+              <ion-input v-model="guardian.cellphone.value" :disabled="guardian.cellphone.disabled" placeholder="Cellphone Number" class="box ion-margin-top" />
+            </ion-col>
+          </ion-row>
+        </ion-col>
+      </ion-row>
+  </ion-grid>
+</template>
+
+<script lang="ts">
+import { IonCheckbox, IonCol, IonGrid, IonInput, IonRow, IonSelect, IonSelectOption } from '@ionic/vue'
+import { defineComponent, PropType, reactive, ref, watch } from 'vue'
+
+export default defineComponent({
+  name: 'PatientRegistrationForm',
+  props: {
+    guardianDetails: {
+      type: Object as PropType<Record<string, any>>,
+      required: true
+    },
+    patientDetails: {
+      type: Object as PropType<Record<string, any>>,
+      required: true
+    },
+  },
+  components: {
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonInput,
+    IonSelect,
+    IonSelectOption,
+    IonCheckbox,
+  },
+  emits: ["updatePatient", "updateGuardian"],
+  setup(props, { emit }) {
+    const estimateAge = ref(false)
+    const guardianNotAvailable = ref(false)
+    const addCustomVillage = ref(false)
+    const addCustomLandmark = ref(false)
+    const guardianPhoneUnknown = ref(false)
+    const patientPhoneUnknown = ref(false)
+    const guardian = reactive(props.guardianDetails)
+    const patient = reactive(props.patientDetails)
+
+    watch(guardianNotAvailable, (isNotAvailable) =>{
+      for (const key in guardian) {
+        guardian[key].value = isNotAvailable ? 'Unknown' : '' 
+        guardian[key].disabled = isNotAvailable
+      }
+    })
+
+    watch(guardianPhoneUnknown, (isUnknown) => {
+      guardian.cellphone.value = isUnknown ? 'Unknown' : ''
+      guardian.cellphone.disabled = isUnknown
+    })
+
+    watch(patientPhoneUnknown, (isUnknown) => {
+      patient.cellphone.value = isUnknown ? 'Unknown' : ''
+      patient.cellphone.disabled = isUnknown
+    })
+
+    watch(guardian, newGuardian => emit("updateGuardian", newGuardian), {
+      deep: true,
+      immediate: true
+    })
+
+    watch(patient, newPatient => emit("updatePatient", newPatient), {
+      deep: true,
+      immediate: true
+    })
+
+    return {
+      estimateAge,
+      guardianNotAvailable,
+      addCustomVillage,
+      addCustomLandmark,
+      guardianPhoneUnknown,
+      patientPhoneUnknown,
+      patient,
+      guardian
+    }
+  },
+})
+</script>
+
+
+<style scoped>
+.box {
+  border-color: #a3a3a3;
+  border-width: thin;
+  border-style: solid;
+  border-radius: 3px;
+}
+
+.checkbox-label {
+  color: rgb(67, 149, 204);
+  font-weight: bold;
+}
+</style>
--- src/apps/EMC/views/PatientRegistration.vue
+++ src/apps/EMC/views/PatientRegistration.vue
@@ -6,140 +6,17 @@
           <h1>New Patient Registration</h1>
         </ion-col>
       </ion-row>
-      <ion-row class="his-card">
-        <ion-col size="12">
-          <ion-title class=" ion-text-center ion-margin-vertical"><b>Personal Details</b></ion-title>
-          <ion-row class="ion-margin-bottom">
-            <ion-col size="4">
-              <ion-label style="margin-bottom: 1em">First Name (*): </ion-label>
-              <ion-input placeholder="First Name" class="box ion-margin-top" />
-            </ion-col>
-            <ion-col size="4">
-              <ion-label class="ion-padding-end">Middle Name: </ion-label>
-              <ion-input class="box ion-margin-top" placeholder="Middle Name" />
-            </ion-col>
-            <ion-col size="4">
-              <ion-label class="ion-padding-end">Family Name (*): </ion-label>
-              <ion-input class="box ion-margin-top" placeholder="Family Name" />
-            </ion-col>
-          </ion-row>
-          <ion-row class="ion-margin-bottom">
-            <ion-col size="7">
-              <ion-row>
-                <ion-col size="12">
-                  <ion-label>
-                    <span v-if="estimateAge">Estimage Age (*):</span>
-                    <span v-else>Date of Birth (*):</span>
-                    <span class="ion-float-right ion-margin-end checkbox-label">
-                      Estimate Date of Birth
-                      <ion-checkbox v-model="estimateAge"></ion-checkbox>
-                    </span>
-                  </ion-label>
-                </ion-col>
-              </ion-row>
-              <ion-row>
-                <template v-if="estimateAge">
-                  <ion-col size="12">
-                    <ion-input class="box" :min="1" type="number" placeholder="Enter age estimate" />
-                  </ion-col>
-                </template>
-                <template v-else>
-                  <ion-col size="4">
-                    <ion-input class="box" :min="1" type="number" placeholder="DD" />
-                  </ion-col>
-                  <ion-col size="4">
-                    <ion-input class="box" :min="1" type="number" placeholder="MM" />
-                  </ion-col>
-                  <ion-col size="4">
-                    <ion-input class="box" :min="1920" type="number" placeholder="YYYY" />
-                  </ion-col>
-                </template>
-              </ion-row>
-            </ion-col>
-            <ion-col size="5" style="padding-top: .4rem;">
-              <ion-label >Gender (*): </ion-label>
-              <ion-select class="box ion-margin-top">
-                <ion-select-option>Male</ion-select-option>
-                <ion-select-option>Female</ion-select-option>
-              </ion-select>
-            </ion-col>
-          </ion-row>
-          <ion-row class="ion-margin-bottom">
-            <ion-col size="12">
-              <ion-label>
-                Cellphone Number (*):
-                <span class="ion-float-right ion-margin-end checkbox-label">
-                  Cellphone Number Unknown? 
-                  <ion-checkbox v-model="patientPhoneUnknown"></ion-checkbox>
-                </span>
-              </ion-label>
-              <ion-input v-model="patient.cellphone.value" :disabled="patient.cellphone.disabled" placeholder="Cellphone Number" class="box ion-margin-top" />
-            </ion-col>
-          </ion-row>
-          <ion-title class=" ion-text-center ion-margin-vertical"><b>Physical Address</b></ion-title>
-          <ion-row class="ion-margin-bottom">
-            <ion-col size="6">
-              <ion-label>
-                Home Village (*):
-                <span class="ion-float-right ion-margin-end checkbox-label">
-                  Village not listed? 
-                  <ion-checkbox v-model="addCustomVillage"></ion-checkbox>
-                </span>
-              </ion-label>
-              <ion-input v-if="addCustomVillage" placeholder="enter village" class="box ion-margin-top" />
-              <ion-select class="box ion-margin-top" placeholder="Select Village" v-else>
-                <ion-select-option>Test Village</ion-select-option>
-                <ion-select-option>Test Village</ion-select-option>
-              </ion-select>
-            </ion-col>
-            <ion-col size="6">
-              <ion-label>
-                Closest Landmark or Plot Number (*):
-                <span class="ion-float-right ion-margin-end checkbox-label">
-                  Landark not listed? 
-                  <ion-checkbox v-model="addCustomLandmark"></ion-checkbox>
-                </span> 
-              </ion-label>
-              <ion-input v-if="addCustomLandmark" placeholder="enter closest landmark or plot number" class="box ion-margin-top" />
-              <ion-select class="box ion-margin-top" placeholder="Select Landmark" v-else>
-                <ion-select-option>Church</ion-select-option>
-                <ion-select-option>Primary School</ion-select-option>
-              </ion-select>
-            </ion-col>
-          </ion-row>
-          <ion-title class=" ion-text-center ion-margin-vertical">
-            <b>Guardian details</b>
-            <span class="ion-margin-start checkbox-label">
-              Guardian Details Unknown? 
-              <ion-checkbox v-model="guardianNotAvailable"></ion-checkbox>
-            </span>
-          </ion-title>
-          <ion-row class="ion-margin-bottom">
-            <ion-col size="4">
-              <ion-label>First Name (*): </ion-label>
-              <ion-input v-model="guardian.givenName.value" :disabled="guardian.givenName.disabled" placeholder="First Name" class="box ion-margin-top" />
-            </ion-col>
-            <ion-col size="4">
-              <ion-label>Family Name (*): </ion-label>
-              <ion-input v-model="guardian.familyName.value" :disabled="guardian.familyName.disabled" class="box ion-margin-top" placeholder="Family Name" />
-            </ion-col>
-            <ion-col size="4">
-              <ion-label>
-                Cellphone Number (*):
-                <span class="ion-float-right ion-margin-end checkbox-label">
-                  Cellphone Number Unknown? 
-                  <ion-checkbox v-model="guardianPhoneUnknown"></ion-checkbox>
-                </span>
-              </ion-label>
-              <ion-input v-model="guardian.cellphone.value" :disabled="guardian.cellphone.disabled" placeholder="Cellphone Number" class="box ion-margin-top" />
-            </ion-col>
-          </ion-row>
-          <ion-row class="ion-margin-top">
-            <ion-col class="ion-margin-top">
-              <ion-button class="searchBtn">Next step</ion-button>
-              <ion-button class="searchBtn" color="warning">Reset</ion-button>
-            </ion-col>
-          </ion-row>
+      <keep-alive>
+        <PatientRegistrationForm
+          :patientDetails="patient"
+          :guardianDetails="guardian"
+          @updatePatient="updatePatient"
+          @updateGuardian="updateGuardian"
+        />
+      </keep-alive>
+      <ion-row class="ion-margin-top">
+        <ion-col class="ion-margin-top">
+          <ion-button class="searchBtn" @click="goNext">Next step</ion-button>
         </ion-col>
       </ion-row>
     </ion-grid>
@@ -153,6 +30,7 @@ import { IonGrid, IonRow, IonCol, IonInput, IonSelect, IonSelectOption, IonCheck
 import { Patientservice } from "@/services/patient_service";
 import GLOBAL_PROP from "@/apps/GLOBAL_APP/global_prop";
 import { toastWarning } from "@/utils/Alerts";
+import PatientRegistrationForm from "../Components/PatientRegistrationForm.vue";
 
 export default defineComponent({
   components: {
@@ -164,15 +42,9 @@ export default defineComponent({
     IonSelect,
     IonSelectOption,
     IonCheckbox,
-  },
+    PatientRegistrationForm
+},
   setup() {
-    const estimateAge = ref(false)
-    const guardianNotAvailable = ref(false)
-    const addCustomVillage = ref(false)
-    const addCustomLandmark = ref(false)
-    const guardianPhoneUnknown = ref(false)
-    const patientPhoneUnknown = ref(false)
-
     const patient = reactive<Record<string, any>>({
       givenName: {
         required: true,
@@ -194,6 +66,12 @@ export default defineComponent({
         disabled: false,
         value: ''
       },
+      birthdate: {
+        required: true,
+        day: '',
+        month: '',
+        year: ''
+      },
       cellphone: {
         required: true,
         value: ''
@@ -202,7 +80,7 @@ export default defineComponent({
         required: true,
         value: ''
       },
-      landMark: {
+      landmark: {
         required: true,
         value: ''
       },
@@ -223,54 +101,34 @@ export default defineComponent({
       },
     })
 
-    watch(guardianNotAvailable, (isNotAvailable) =>{
-      for (const key in guardian) {
-        guardian[key].value = isNotAvailable ? 'Unknown' : '' 
-        guardian[key].disabled = isNotAvailable
-      }
-    })
+    const updateGuardian = (newGauardian: Record<string, any>) => {
+      Object.assign(guardian, newGauardian)
+    }
+    
+    const updatePatient = (newPatient: Record<string, any>) => {
+      Object.assign(patient, newPatient)
+    }
 
-    watch(guardianPhoneUnknown, (isUnknown) => {
-      guardian.cellphone.value = isUnknown ? 'Unknown' : ''
-      guardian.cellphone.disabled = isUnknown
-    })
-
-    watch(patientPhoneUnknown, (isUnknown) => {
-      patient.cellphone.value = isUnknown ? 'Unknown' : ''
-      patient.cellphone.disabled = isUnknown
-    })
+    const goNext = () => {
+      console.log(patient)
+    }
 
     return {
       patient,
       guardian,
-      guardianNotAvailable,
-      addCustomVillage,
-      addCustomLandmark,
-      guardianPhoneUnknown,
-      patientPhoneUnknown,
-      estimateAge
+      updatePatient,
+      updateGuardian,
+      goNext,
     };
   },
 });
 </script>
 
 <style scoped>
-.box {
-  border-color: #a3a3a3;
-  border-width: thin;
-  border-style: solid;
-  border-radius: 3px;
-}
-
 .searchBtn {
   height: 50px !important;
   margin-top: 0;
   margin-bottom: 0;
   margin-right: .5rem;
-}
-
-.checkbox-label {
-  color: rgb(67, 149, 204);
-  font-weight: bold;
 }
 </style>
