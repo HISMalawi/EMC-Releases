a3e97537d638cc05be9bcea59277d7e2d42cf0f2
Daniel Justin <danieljmuleke@gmail.com>
2022-03-17 15:28:37 +0200

    Implemented a searchable select input

---
+++ src/apps/EMC/Components/inputs/SearchableSelectInput.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    style="min-height: 40px; padding: 0.5rem"
+    :value="value"
+    @click="showOptions"
+  >
+    <span style="" v-if="value">{{ value }}</span>
+    <span v-else style="color: #a3a3a3;">{{ placeholder }}</span>
+    <ion-icon :icon="caretDown" style="color: #a3a3a3; float: right;"></ion-icon>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Option } from "@/components/Forms/FieldInterface";
+import SearchableList from '@/apps/EMC/Components/SearchableList.vue'
+import { caretDown } from "ionicons/icons"
+import { IonIcon, modalController } from '@ionic/vue'
+
+export default defineComponent({
+  name: "SearchableSelectInput",
+  components: {
+    IonIcon,
+  },
+  props: {
+    value: {
+      type: String,
+    },
+    asyncOptions: {
+      type: Function as PropType<(filter: string) => Promise<Option[]>>,
+      required: false,
+    },
+    options: {
+      type: Object as PropType<Option[]>,
+      default: () => [],
+    },
+    multiSelect: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+      default: "Select Items",
+    },
+  },
+  emits: ["onSelect"],
+  setup(props, { emit }) {
+    const showOptions = async () => {
+      const modal = await modalController.create({
+        component: SearchableList,
+        backdropDismiss: false,
+        cssClass: '',
+        componentProps: {
+          asyncOptions: props.asyncOptions,
+          options: props.options,
+          title: props.placeholder,
+          multiSelect: props.multiSelect
+        }
+      })
+
+      modal.present()
+      const { data } = await modal.onDidDismiss()
+      if(data) emit("onSelect", data)
+    }
+    return {
+      caretDown,
+      showOptions
+    }
+  },
+})
+</script>
