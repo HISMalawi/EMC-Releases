cefea1662fecb1b67e07773e0de712cf5931a704
Andrew Mfune <andiemfune@gmail.com>
2022-03-21 07:43:37 +0200

    Refactored code to use HisStandardForm

--- src/apps/ANC/Views/Encounters/SocialHistory.vue
+++ src/apps/ANC/Views/Encounters/SocialHistory.vue
@@ -1,178 +1,93 @@
 <template>
     <ion-page> 
-        <ion-header> 
-            <ion-toolbar> 
-                <ion-title> Social History </ion-title>
-            </ion-toolbar>
-        </ion-header>
-        <ion-content>
-            <view-port>
-                <ion-grid class="view-port-content">
-                    <ion-row>
-                        <ion-col style="width:100%"  v-for="(item, index) in options" :key="index" size="6">
-                            <ion-card class="his-card"> 
-                                <ion-card-header> 
-                                    <ion-card-title>{{item.title}}</ion-card-title>
-                                </ion-card-header>
-                                <ion-card-content> 
-                                    <ion-list> 
-                                        <ion-radio-group v-model="item.value">
-                                            <ion-item v-for="(option, optionIndex) in item.options" :key="optionIndex"> 
-                                                <ion-radio :value="option.value" slot="start"/>
-                                                <ion-label>{{option.label}}</ion-label>
-                                            </ion-item>
-                                        </ion-radio-group>
-                                    </ion-list>
-                                </ion-card-content>
-                            </ion-card>
-                        </ion-col>
-                    </ion-row>
-                </ion-grid>
-            </view-port>
-        </ion-content>
-        <ion-footer>
-            <ion-toolbar color="dark">
-                <ion-button 
-                    @click="onCancel" 
-                    slot="start" 
-                    color="danger" 
-                    size="large"> 
-                    Cancel
-                </ion-button>
-                <ion-button 
-                    :disabled="isSubmitting || !dataComplete" 
-                    @click="onFinish" 
-                    slot="end" 
-                    color="success" 
-                    size="large">
-                    Finish
-                </ion-button>
-            </ion-toolbar>
-        </ion-footer>
-    </ion-page>    
+        <his-standard-form
+            :fields="fields"
+            :skipSummary="true"
+            :onFinishAction="onFinish"
+        />
+    </ion-page>
 </template>
 <script lang="ts">
 import EncounterMixinVue from '@/views/EncounterMixin.vue'
 import { defineComponent } from 'vue'
-import {
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardContent,
-    IonHeader,
-    IonFooter,
-    IonTitle,
-    IonContent,
-    IonGrid,
-    IonPage,
-    IonRow,
-    IonCol,
-    IonItem,
-    IonButton,
-    IonRadio,
-    IonList,
-    IonLabel,
-    IonToolbar,
-    IonRadioGroup,
-} from "@ionic/vue"
-import { alertConfirmation, toastDanger } from '@/utils/Alerts'
-import ViewPort from "@/components/DataViews/ViewPort.vue"
 import { AncSocialHistoryService } from "@/apps/ANC/Services/anc_social_history_service"
+import { IonPage } from "@ionic/vue"
+import { Field, Option } from '@/components/Forms/FieldInterface'
+import { FieldType } from '@/components/Forms/BaseFormElements'
 
 export default defineComponent({
-    components: {
-        ViewPort,
-        IonCard,
-        IonItem,
-        IonList,
-        IonLabel,
-        IonCardHeader,
-        IonCardTitle,
-        IonCardContent,
-        IonHeader,
-        IonTitle,
-        IonGrid,
-        IonPage,
-        IonRow,
-        IonCol,
-        IonContent,
-        IonFooter,
-        IonButton,
-        IonRadio,
-        IonToolbar,
-        IonRadioGroup
-    },
+    components: { IonPage },
     mixins: [EncounterMixinVue],
     data: () => ({
-        isSubmitting: false as boolean,
         service: {} as any,
-        options: [] as any
     }),
     watch: {
         ready: {
             handler(ready: boolean){
                 if (ready) {
                     this.service = new AncSocialHistoryService(this.patientID, this.providerID)
+                    this.fields = this.getFields()
                 }
             }
         }
     },
-    computed: {
-        dataComplete(): boolean {
-            return this.options.map((o: any) => o.value ? true : false).every(Boolean)
-        }
-    },
     methods: {
-        async onCancel() {
-            const ok = await alertConfirmation('Are you sure you want to cancel?')
-            if (ok) this.gotoPatientDashboard()
+        async onFinish(f: any, c: any){
+            const obs = await this.resolveObs(c)
+            await this.service.createEncounter()
+            await this.service.saveObservationList(obs)
+            this.nextTask()
         },
-        async onFinish(){
-            this.isSubmitting = true
-            try {
-                await this.service.createEncounter()
-                const obs = this.options.map(
-                    async ({concept, value}: any) => await this.service.buildValueCoded(concept, value)
-                )
-                await this.service.saveObservationList((await Promise.all(obs)))
-                this.nextTask()
-            } catch (e) {
-                toastDanger(e)
-            }
-            this.isSubmitting = false
+        getFields(): Field[] {
+            return [
+                {
+                    id: 'history',
+                    helpText: 'Social history',
+                    type: FieldType.TT_MULTI_SELECT_GRID,
+                    validation: (v: Option[]) => {
+						return v.map(v => v.value==='').some(Boolean)
+							? ['Please complete selection!!'] 
+							: null
+                    },
+                    computedValue: (v: Option[]) => v.filter(d => d.value != '')
+                        .map(d => this.service.buildValueCoded(d.other.concept, d.value)),
+                    options: () => {
+                        return [
+                            {
+                                label: 'Currently Smoking',
+                                value: '',
+                                other: {
+                                    concept: 'patient smokes',
+                                    options: this.yesNoOptions()
+                                }
+                            },
+                            {
+                                label: 'Currently taking alcohol',
+                                value: '',
+                                other: {
+                                    concept: 'Currently taking alcohol',
+                                    options: this.yesNoOptions()
+                                }
+                            },
+                            {
+                                label: 'Marital status',
+                                value: '',
+                                other: {
+                                    concept: 'Marital status',
+                                    options: [
+                                        this.toOption('Never Married'),
+                                        this.toOption('Married'),
+                                        this.toOption('Seperated'),
+                                        this.toOption('Divorced')
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
         }
-    },
-    created() {
-        this.options = [
-            { 
-                title: 'Currently Smoking',
-                concept: 'patient smokes',
-                value: null,
-                options: this.yesNoOptions()
-            },
-            {
-                title: 'Currently taking alcohol',
-                concept: 'Currently taking alcohol',
-                value: null,
-                options: this.yesNoOptions()
-            },
-            {
-                title: 'Marital status',
-                concept: 'Marital status',
-                value: null,
-                options: [
-                    this.toOption('Never Married'),
-                    this.toOption('Married'),
-                    this.toOption('Seperated'),
-                    this.toOption('Divorced')
-                ]
-            }
-        ]
     }
 })
 </script>
-<style scoped>
-    .his-card {
-        padding: 2px;
-    }
-</style>
+
