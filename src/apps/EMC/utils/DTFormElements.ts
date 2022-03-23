43dce567ad46ed8f3be0f54554ba26d3daf8aae8
Daniel Justin <danieljmuleke@gmail.com>
2022-03-23 07:43:58 +0200

    Added reusable input form elements options

--- src/apps/EMC/utils/DTFormElements.ts
+++ src/apps/EMC/utils/DTFormElements.ts
@@ -1,6 +1,35 @@
 import { Option } from "@/components/Forms/FieldInterface";
 
+function strToOptions(str: string[]) {
+  return str.map<Option>(s => ({
+    value: s,
+    label: s
+  }))
+}
+
 export const yesNoOptions: Option[] = [
   { label: "Yes", value: "Yes" },
   { label: "No", value: "No" }
+]
+
+export const genderOptions: Option[] = [
+  { label: "Male", value: "M" },
+  { label: "Female", value: "F" },
+];
+
+export const tbStatusOptions: Option[] = [
+  ...strToOptions([
+    "TB NOT suspected",
+    "TB suspected",
+    "Confirmed TB NOT on treatment",
+    "Confirmed TB on treatment"
+  ])
+]
+
+export const HIVTestOptions = [
+  ...strToOptions([
+    "Rapid Antibody Test",
+    "DNA PCR",
+    "Not Done",
+  ])
 ]
