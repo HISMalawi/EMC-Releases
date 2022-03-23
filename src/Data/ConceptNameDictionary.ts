ff8220c2da71923fd78502bcbeb100b437e1997e
Daniel Justin <danieljmuleke@gmail.com>
2022-03-23 07:42:39 +0200

    Added TB status concepts

--- src/Data/ConceptNameDictionary.ts
+++ src/Data/ConceptNameDictionary.ts
@@ -45,6 +45,16 @@ export default [
    },
    {
       "categories": [],
+      "concept_id": 7455,
+      "name": "TB suspected"
+   },
+   {
+      "categories": [],
+      "concept_id": 7454,
+      "name": "TB NOT suspected"
+   },
+   {
+      "categories": [],
       "concept_id": 7458,
       "name": "Confirmed TB on treatment"
    },
