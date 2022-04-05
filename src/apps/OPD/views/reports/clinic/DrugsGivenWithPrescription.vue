d2a24748b27c3bdc8440bcd9a550de509ae238aa
petroskayange <kayangepetros@gmail.com>
2022-04-05 15:33:29 +0200

    additional of High level button and high level popup

--- src/apps/OPD/views/reports/clinic/DrugsGivenWithPrescription.vue
+++ src/apps/OPD/views/reports/clinic/DrugsGivenWithPrescription.vue
@@ -5,10 +5,9 @@
       :rows="rows"
       :fields="fields"
       :columns="columns"
-      :itemsPerPage="12"
       :period="period"
       :onReportConfiguration="init"
-      paginated
+      :customBtns="customBtns"
     ></report-template>
   </ion-page>
 </template>
@@ -19,7 +18,9 @@ import { OpdReportService } from "@/apps/OPD/services/opd_report_service"
 import ReportTemplate from "@/views/reports/BaseTableReport.vue"
 import table, { ColumnInterface, RowInterface } from "@/components/DataViews/tables/ReportDataTable"
 import HisDate from '@/utils/Date'
+import { modalController } from '@ionic/core'
 import ReportMixin from '@/apps/ART/views/reports/ReportMixin.vue'
+import SummaryModal from "@/apps/OPD/components/DrugsGivenSummaryModal.vue";
 import { IonPage } from "@ionic/vue";
 
 export default defineComponent({
@@ -28,6 +29,8 @@ export default defineComponent({
   data: () => ({
     title: 'OPD medication given (with prescription)',
     rows: [] as RowInterface[][], 
+    reportData: [] as any,
+    highLevelStats: [] as any,
     columns: [[
       table.thTxt('First Name'),
       table.thTxt('Last Name'),
@@ -36,10 +39,19 @@ export default defineComponent({
       table.thTxt('Drug'),
       table.thTxt('Quantity'),
       table.thTxt('Date'),
-    ]] as ColumnInterface[][]
+    ]] as ColumnInterface[][],
+    customBtns: [] as any
   }),
   created(){
     this.fields = this.getDateDurationFields()
+    this.customBtns.push({
+      name: "High level view",
+      size: "large",
+      slot: "start",
+      color: "primary",
+      visible: true,
+      onClick: async () => this.showModal()
+    })
   },
   methods: {
     async init(_: any, config: any){
@@ -47,6 +59,7 @@ export default defineComponent({
       reportService.setStartDate(config.start_date)
       reportService.setEndDate(config.end_date)
       this.period = reportService.getDateIntervalPeriod()
+      this.reportData = await reportService.getDrugsGivenWithPrescription();
       this.rows = this.buildRows((await reportService.getDrugsGivenWithPrescription()))
     },
     buildRows(data: any[]): RowInterface[][] {
@@ -60,6 +73,45 @@ export default defineComponent({
         table.td(record.quantity),
         table.td(HisDate.toStandardHisDisplayFormat(record.date)),
       ])
+    },
+    async showModal() {
+     
+      this.buildHighlevelView();
+      const data = [
+        { count: 1 ,label: "New patient", value: [...this.reportData.filter((d: any) => d.visit_type === 'New patient')].length },
+        { count: 1 ,label: "Referral", value: [...this.reportData.filter((d: any) => d.visit_type === 'Referral')].length },
+        { count: 1 ,label: "Revisiting", value: [...this.reportData.filter((d: any) => d.visit_type === 'Revisiting')].length }
+      ]
+      console.log('show modal',this.highLevelStats)
+      const modal = await modalController.create({
+        component: SummaryModal,
+        backdropDismiss: true,
+        cssClass: 'action-sheet-modal',
+        componentProps: {
+          list: this.highLevelStats
+        } 
+      })
+      modal.present()
+    },
+    buildHighlevelView(){
+      const data: any = this.reportData;
+        for(let value in data){
+            const drug = data[value]['drug_name']
+            let drugName = data.filter((item: any)=>{
+                return item.drug_name == drug
+            })
+            
+            let quantity =0;
+            drugName = drugName.map((data: any) =>{
+                quantity += data.quantity
+                return {label : data.drug_name, value : quantity}
+            })[drugName.length - 1]
+
+            this.highLevelStats.push(drugName);
+        }
+
+        this.highLevelStats = this.highLevelStats.filter((v: any,i: any,a: any)=>a.findIndex((v2: any)=>['place','label'].every(k=>v2[k] ===v[k]))===i)
+        console.log('build high',this.highLevelStats)
     }
   },
 })
