b814e7790b6d661df5f720a2396c11be27f04e43
Andrew Mfune <andiemfune@gmail.com>
2022-05-26 15:52:18 +0200

    Added ART Tasks and fixed some workflowIDs

--- src/apps/ANC/Config/AncProgramActivites.ts
+++ src/apps/ANC/Config/AncProgramActivites.ts
@@ -4,6 +4,7 @@ export const PRIMARY_ACTIVITIES: TaskInterface[] = [
     {
        id: 'anc dispensing',
        name: 'TTV VACCINATION',
+       encounterTypeName: 'DISPENSING',
        icon: 'prescription/medication.png'
     },
     {
@@ -24,6 +25,8 @@ export const PRIMARY_ACTIVITIES: TaskInterface[] = [
     {
         id: 'anc examination',
         name: 'Examination',
+        workflowID: 'ANC examination',
+        encounterTypeName: 'ANC EXAMINATION',
         icon: 'diagnosis.png'
     },
     {
@@ -48,13 +51,83 @@ export const PRIMARY_ACTIVITIES: TaskInterface[] = [
     },
     {
         id: 'anc treatment',
-        workflowID: 'anc treatment',
-        name: 'Give Drugs',
+        workflowID: 'Give drugs',
+        name: 'anc treatment',
+        encounterTypeName: 'TREATMENT',
         icon: 'dispensing.png'
     },
     {
         id: 'art_followup',
-        name: 'ART FOLLOWUP',
+        name: 'art_followup',
+        workflowID: 'ARV Follow Up',
         icon: 'pmtct.png'
+    },
+    {
+        id: "vitals",
+        name: "Vitals",
+        icon: "vitals.png",
+        taskCompletionChecklist: [
+          'Weight'
+        ]
+    },
+    {
+        id: "appointment",
+        name: "Manage Appointments",
+        encounterTypeName: "APPOINTMENT",
+        workflowID: "Manage appointment",
+        icon: "appointment.png"
+    },
+    {
+        id: "hiv clinic registration",
+        name: "Hiv clinic registration",
+        workflowID: "HIV first visits",
+        icon: "registration.png",
+        condition: () => false
+    },
+    {
+        id: "hiv reception",
+        name: "HIV reception",
+        workflowID: "HIV reception visits",
+        icon: "reception.png",
+        condition: () => false
+    },
+    {
+        id: "hiv staging",
+        name: "HIV staging",
+        workflowID: "HIV staging visits",
+        icon: "hiv-staging.png",
+        condition: () => false
+    },
+    {
+        id: "hiv clinic consultation",
+        name: "HIV clinic consultation",
+        workflowID: "HIV clinic consultations",
+        taskCompletionChecklist: [
+          'Medication orders'
+        ],
+        icon: "consultation.png",
+        condition: () => false
+    },
+    {
+        id: "art adherence",
+        name: "ART adherence",
+        workflowID: "ART adherence",
+        icon: "adherence.png",
+        condition: () => false
+    },
+    {
+        id: "treatment",
+        name: "Treatment",
+        workflowID: "Prescriptions",
+        icon: "prescription.png",
+        condition: () => false
+    },
+    {
+        id: "dispensing",
+        name: "Drug Dispensations",
+        encounterTypeName: "DISPENSING",
+        workflowID: "Drug Dispensations",
+        icon: "dispensing.png",
+        condition: () => false
     }
 ]
