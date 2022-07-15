d1fc4501b5c0d2bf30460931d02f9b53e191bcfc
Daniel Justin <danieljmuleke@gmail.com>
2022-07-15 06:52:34 +0200

    Added modal utility functions

---
+++ src/utils/modal.ts
@@ -0,0 +1,18 @@
+import { modalController } from "@ionic/vue";
+
+export const modal = {
+  async show(component: any, componentProps?: Record<string, any>, cssClass = 'custom-modal', backdropDismiss = true) {
+    const m = await modalController.create({
+      component,
+      cssClass,
+      backdropDismiss,
+      componentProps,
+    });
+    m.present();
+    const { data } = await m.onWillDismiss();
+    if(data) return data;
+  },
+  async hide() {
+    await modalController.dismiss();
+  }
+}
