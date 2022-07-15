1dfca9e971272902fbbd91dd7a48a5b1d69b530d
Daniel Justin <danieljmuleke@gmail.com>
2022-07-15 06:52:52 +0200

    defined emc events

---
+++ src/apps/EMC/interfaces/emc_event.ts
@@ -0,0 +1,5 @@
+export enum EmcEvents {
+  RELOAD_PATIENT_DATA = 'reload_patient_data',
+  RELOAD_PATIENT_VISIT_DATA = 'reload_patient_visit_data',
+  RELOAD_GUARDIAN_DATA = 'reload_guardian_data',
+}
