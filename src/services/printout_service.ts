3d60a21e2c7108b5fa609b9155cbefe4b83008f5
Daniel Justin <danieljmuleke@gmail.com>
2022-07-21 13:30:02 +0200

    Implemented Bluetooth printing service

--- src/services/printout_service.ts
+++ src/services/printout_service.ts
@@ -1,9 +1,11 @@
 import { Service } from "./service";
 import ApiClient from "./api_client";
 import usePlatform from "@/composables/usePlatform";
-import { toastWarning } from "@/utils/Alerts";
+import { toastSuccess, toastWarning } from "@/utils/Alerts";
 import { EventChannels } from "@/utils/EventBus";
 import EventBus from "@/utils/EventBus";
+import { BluetoothSerial } from "@awesome-cordova-plugins/bluetooth-serial";
+import { optionsActionSheet } from '@/utils/ActionSheets';
 
 export class PrintoutService extends Service {
     constructor() {
@@ -49,7 +51,18 @@ export class PrintoutService extends Service {
                 toastWarning(e as any, 3000)
             }
         } else {
-            toastWarning('Sorry, your Platform does not support Label printing. Bye!')
+            try {
+                const printer = await this.getDefaultPrinter();
+                if (!printer) return
+                if(showPrintImage) EventBus.emit(EventChannels.SHOW_MODAL, 'zebra-modal')  
+                BluetoothSerial.connect(printer).subscribe(async () => {
+                    BluetoothSerial.write(await Service.getText(url))
+                        .then(() => BluetoothSerial.disconnect())
+                })
+
+            } catch (error) {
+                toastWarning(`${error}`)
+            }
         }
     }
 
@@ -59,5 +72,40 @@ export class PrintoutService extends Service {
 
     async printDrug(drugId: number, quantity: number) {
         await this.printLbl(`drugs/${drugId}/barcode?quantity=${quantity}`)
+    }
+
+    setDefaultPrinter(printer: string) {
+        sessionStorage.setItem('defaultPrinter', printer)
+    }
+
+    async getDefaultPrinter() {
+        const printer = sessionStorage.getItem('defaultPrinter')
+        if (printer) return printer
+        return this.selectDefaultPrinter()
+    }
+
+    async selectDefaultPrinter(): Promise<string> {
+        const printers: any[] = await BluetoothSerial.list()
+        if (printers.length === 0) {
+            toastWarning('No printers found. Please connect a printer.')
+            return ''
+        }
+        const option = await optionsActionSheet(
+            "Select Printer",
+            "Please, select default printer",
+            printers.map(p => p.name + ` (${p.address})`),
+            [
+                { name: 'Cancel', slot:'start', color: 'danger' },
+                { name: 'Continue', slot: 'end', role: 'action' }
+            ]
+        )
+
+        if(option.action === 'Cancel') return ''
+
+        const printer = printers.find(p => p.name +` (${p.address})` === option.selection)
+        
+        this.setDefaultPrinter(printer.address)
+
+        return printer.address
     }
 }
