4e72601b620584c64e99ad35bcee2a0705e8da2b
Andrew Mfune <andiemfune@gmail.com>
2022-07-27 12:31:59 +0200

    Moved guardian ammendment in service

--- src/services/relations_service.ts
+++ src/services/relations_service.ts
@@ -9,6 +9,15 @@ export class RelationsService extends Service {
 		return Service.void(`people/${patientID}/relationships/${relationID}`, { reason })
 	}
 
+    static async amendRelation(patientID: number, guardianID: number, curRelationID: number, newRelationTypeID: number) {
+        await RelationsService.voidRelation(patientID, curRelationID,
+            'Updating guardian relationship'
+        )
+        return RelationsService.createRelation(
+            patientID, guardianID, newRelationTypeID
+        )
+    }
+
     static getRelations() {
         return super.getJson('types/relationships')
     }
