20470c84b378b3163388989a3dda3a72b1a35b93
Andrew Mfune <andiemfune@gmail.com>
2022-08-04 09:52:29 +0200

    Initial implementation of radiology report service

--- src/apps/RADIOLOGY/services/radiology_report_service.ts
+++ src/apps/RADIOLOGY/services/radiology_report_service.ts
@@ -1,7 +1,34 @@
 import { Service } from "@/services/service"
 
 export class RadiologyReportService extends Service {
-    constructor() {
-        super() 
+    startDate: string
+    endDate: string
+    date: string
+    name: string
+    programID: number
+    constructor(
+        name: string, 
+        startDate: string, 
+        endDate: string, 
+        date=Service.getSessionDate(), 
+        programID=Service.getProgramID()
+        ) {
+        super()
+        this.startDate = startDate
+        this.endDate = endDate
+        this.date = date
+        this.name = name
+        this.programID = programID
+    }
+
+    requestReport(params: any={}) {
+        return Service.getJson('radiology_reports', {
+            'program_id': this.programID,
+            'report_name': this.name,
+            'start_date': this.startDate,
+            'end_date': this.endDate,
+            'date': this.date,
+            ...params
+        })
     }
 }
