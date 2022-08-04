c7b5b5b2dbce61ca195462ea49a9e6eb60451ba5
Andrew Mfune <andiemfune@gmail.com>
2022-08-04 16:14:16 +0200

    Added config to hide medication runout date on Appointments

--- src/apps/RADIOLOGY/views/encounters/RadiologyAppointment.vue
+++ src/apps/RADIOLOGY/views/encounters/RadiologyAppointment.vue
@@ -70,6 +70,7 @@ export default defineComponent({
           },
           config: {
             patientAge: this.patient.getAge(),
+            hideRunoutDate: true,
             hiddenFooterBtns: [
               'Clear'
             ]
