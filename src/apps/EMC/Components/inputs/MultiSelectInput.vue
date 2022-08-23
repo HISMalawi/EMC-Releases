94c466d2e597200d243baf7c49875e92e6a5dc7a
Daniel Justin <danieljmuleke@gmail.com>
2022-08-23 21:21:50 +0200

    Implemented taggable and searchable multiselect input UI

---
+++ src/apps/EMC/Components/inputs/MultiSelectInput.vue
@@ -0,0 +1,196 @@
+<template>
+  <ion-label class="ion-padding-bottom bold" v-if="model.label"> 
+    <span>{{ model.label }}</span>
+    <span class="ion-float-right ion-margin-end checkbox-label" v-if="allowCustom">
+      Add Custom {{ model.label }}?
+      <ion-checkbox v-model="isCustom" @ionChange="model.value = ''"></ion-checkbox>
+    </span>
+  </ion-label>
+  <div class="ion-margin-top outer-input-box box-input" :class="{'box-input-error': model.error }">
+    <div class="inner-input-box">
+      <div style="display: flex;" @click="showOptions = true">
+        <ion-chip v-for="(tag, index) of tags" :key="index" color="secondary">
+          <ion-label>{{ tag.label }}</ion-label>
+        </ion-chip>
+        <ion-input v-if="searchable" v-model="filter" class="search-input" />
+      </div>
+      <ion-list class="input-options" v-if="showOptions">
+        <ion-item v-for="(option, index) of filteredOptions" :key="index" :lines="index+1 === filteredOptions.length ? 'none': ''">
+          <ion-checkbox class="input-option-checkbox" slot="start" v-model="option.isChecked" v-if="multiple"></ion-checkbox>
+          <ion-label>{{ option.label }}</ion-label>
+        </ion-item>
+      </ion-list>
+      <ion-icon :icon="showOptions ? chevronUp : chevronDown" class="input-icon"></ion-icon>
+    </div>
+  </div>
+  <ion-note v-if="model.error" color="danger">{{ model.error }}</ion-note>
+</template>
+<script lang="ts">
+import { IonCheckbox, IonIcon, IonInput, IonLabel, IonNote } from "@ionic/vue";
+import { computed, defineComponent, onBeforeMount, onMounted, PropType, ref } from "vue";
+import { DTForm, DTFormField } from "../../interfaces/dt_form_field";
+import { Option } from '@/components/Forms/FieldInterface';
+import { chevronDown, chevronUp } from "ionicons/icons"
+import { genderOptions, tbStatusOptions } from "../../utils/DTFormElements";
+import { toastSuccess } from "@/utils/Alerts";
+
+export default defineComponent({
+  name: "MultiSelectInput",
+  props: {
+    modelValue: {
+      type: Object as PropType<DTFormField>,
+      default: () => ({}),
+    },
+    form: {
+      type: Object as PropType<DTForm>,
+      default: () => ({}),
+    },
+    options: {
+      type: Array as PropType<Option[]>,
+      default: () => [],
+    },
+    asyncOptions: {
+      type: Function as PropType<(filter: string) => Promise<Option[]>>,
+      required: false,
+    },
+    allowCustom: {
+      type: Boolean,
+      default: false
+    },
+    searchable: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    multiple: {
+      type: Boolean,
+      default: true
+    }
+  },
+  components: {
+    IonLabel,
+    IonInput,
+    IonNote,
+    IonIcon,
+    IonCheckbox,
+  },
+  emits: ["update:modelValue"],
+  setup(props, { emit}) {
+    const isCustom = ref(false);
+    const selectedOption = ref<Option>();
+    const showOptions = ref(false)
+    const tags = ref<Option[]>(genderOptions)
+    const filter = ref('')
+    const filteredOptions = ref<Option[]>(tbStatusOptions)
+    const model = computed({
+      get: () => props.modelValue,
+      set: (value) => emit("update:modelValue", value)
+    })
+
+    const validate = async () => {
+      if (model.value.required && !model.value.value) {
+        return model.value.error = "This field is required";
+      }
+      if (model.value.validation) {
+        const errors = await model.value.validation({label: model.value.value, value: model.value.value}, props.form);
+        if (errors && errors.length) {
+          return model.value.error += errors.toString();
+        }
+      }
+      return model.value.error = "";
+    };
+
+    const diselectOption = (item: Option) => {
+      item.isChecked = false
+      toastSuccess("item has been removed successfully")
+    }
+
+    onMounted(() => {
+      if (model.value.value) {
+        const defaultOption = props.options.find(option => option.value === model.value.value)
+        if (defaultOption) {
+          selectedOption.value = defaultOption
+        }else {
+          selectedOption.value = {
+            label: model.value.value,
+            value: model.value.value
+          }
+        }
+      } else {
+        model.value.value = 0
+      }
+
+      addEventListener('click', (e: any) => {
+        const isClosest = e.target.closest('.inner-input-box')
+        if(!isClosest && showOptions) showOptions.value = false
+      })
+    });
+
+    onBeforeMount(() => {
+      removeEventListener('click', e => console.log(e))
+    })
+
+    return {
+      validate,
+      diselectOption,
+      model,
+      isCustom,
+      chevronDown,
+      chevronUp,
+      selectedOption,
+      showOptions,
+      filteredOptions,
+      filter,
+      tags,
+    };
+  }
+});
+</script>
+
+<style scoped>
+.outer-input-box {
+  width: 100%; 
+  height: 2.5rem; 
+  background-color: #fff;
+}
+
+.inner-input-box {
+  position: relative; 
+  width: 100%; 
+}
+
+.search-input {
+  height: 2.5rem;
+  width: 100%;
+  display: inline-block;
+}
+
+.input-options {
+  position: absolute;
+  width: 100%;
+  max-height: 24rem;
+  overflow-y: auto;
+  z-index: 1000;
+  border-radius: 0.25rem;
+  border-top-left-radius: 0px;
+  border-top-right-radius: 0px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+}
+.input-icon {
+  font-size: 12px !important;
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 2.5rem;
+  width: 1rem;
+  color: rgb(138, 138, 138);
+  padding-right: .5rem;
+}
+
+.input-option-checkbox {
+  --size: 18px !important;
+}
+</style>
