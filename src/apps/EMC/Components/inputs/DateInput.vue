1daee076701ab325266f6e3cb51e0d0ad00ed7ac
Daniel Justin <danieljmuleke@gmail.com>
2022-08-30 09:49:21 +0200

    [BugFix] corrected default date value

--- src/apps/EMC/Components/inputs/DateInput.vue
+++ src/apps/EMC/Components/inputs/DateInput.vue
@@ -106,7 +106,7 @@ export default defineComponent({
     const isEstimated = ref(false);
     const age = ref<number>();
     const day = ref<number | undefined>(props.modelValue.value ? dayjs(props.modelValue.value).date() : undefined);
-    const month = ref<number>(props.modelValue.value ? dayjs(props.modelValue.value).month() : 0);
+    const month = ref<number>(props.modelValue.value ? dayjs(props.modelValue.value).month() + 1 : 0);
     const year = ref<number | undefined>(props.modelValue.value ? dayjs(props.modelValue.value).year() : undefined);
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
