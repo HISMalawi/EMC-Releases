30ac178d153cb586c2e3103ef188cd8de65f23b0
Daniel Justin <danieljmuleke@gmail.com>
2022-09-09 09:38:27 +0200

    Splitted 3hp options to show both old and new 3hp

--- src/apps/ART/views/encounters/Consultation.vue
+++ src/apps/ART/views/encounters/Consultation.vue
@@ -1276,11 +1276,14 @@ export default defineComponent({
             obs: this.consultation.buildValueText("Previous TB treatment history", data.value)
           }),
           options: () => this.mapStrToOptions([
-            "Currently on IPT", 
-            "Currently on 3HP",
+            "Currently on IPT",
+            "Currently on 3HP (RFP + INH)",
+            "Currently on INH 300 / RFP 300 (3HP)",
             "Complete course of 3HP in the past (3 months RFP+INH)",
             "Complete course of IPT in the past (min. 6 months of INH)",
-            "Aborted course of 3HP or IPT in the past",
+            "Aborted course of 3HP (RFP + INH) in the past",
+            "Aborted course of INH 300 / RFP 300 (3HP) in the past",
+            "Aborted course of IPT in the past",
             "Never taken IPT or 3HP"
           ])
         },
