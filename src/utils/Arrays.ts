7584a9b4e17ff64b225b987b0b9f09895736962f
Daniel Justin <danieljmuleke@gmail.com>
2022-10-04 15:59:01 +0200

    [Doc] added function documentation

--- src/utils/Arrays.ts
+++ src/utils/Arrays.ts
@@ -1,3 +1,10 @@
+/**
+ * A function that remove duplicates from an array
+ * 
+ * @param arr list of items with possible duplicates
+ * @param iteratee a key or list of keys to be used to remove duplicates
+ * @returns list of unique items
+ */
 export function uniqueBy(arr: any[], iteratee: string | string[]) {
   return [...new Map(arr.filter(Boolean).map(item => {
     const uniqueKey = Array.isArray(iteratee) 
