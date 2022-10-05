53d68584015e716cb8c5343875b8e9326ea7408c
Andrew Mfune <andiemfune@gmail.com>
2022-10-05 14:40:10 +0200

    Added device polling to refresh device list every 5000 milliseconds

--- src/views/PrinterSettings.vue
+++ src/views/PrinterSettings.vue
@@ -14,9 +14,31 @@ import { PrintoutService } from "@/services/printout_service";
 import { isEmpty } from "lodash";
 import { PrinterDescription } from "@/interfaces/PrintServiceInterfaces";
 import { loadingController } from "@ionic/core";
+import { toastDanger, toastWarning } from "@/utils/Alerts";
 
 export default defineComponent({
   components: { HisStandardForm },
+  data() {
+    return {
+      printFieldContext: null as any,
+      printerService: {} as PrintoutService,
+      defaultPrinter: {} as Record<string, any>,
+      refreshKey: 0,
+      fields: [] as any,
+      refreshInterval: null as any,
+      isRefreshingDevices: false as boolean
+    };
+  },
+  async mounted() {
+    this.printerService = new PrintoutService();
+    this.defaultPrinter = await this.printerService.getDefaultPrinter();
+    this.fields = this.getFields();
+  },
+  unmounted() {
+    if (this.refreshInterval) {
+      clearInterval(this.refreshInterval)
+    }
+  },
   methods: {
     async onFinish() {
       this.$router.back();
@@ -58,17 +80,35 @@ export default defineComponent({
                 color: "warning",
                 visible: true,
                 onClick: async () => {
+                  if (this.isRefreshingDevices) {
+                    return toastWarning('Please wait...')
+                  }
                   if (this.printFieldContext) {
                     (await loadingController.create({
                       backdropDismiss: false,
                       message: 'Please wait...'
                     })).present();
-                    await this.printFieldContext.init()
+                    this.isRefreshingDevices = true
+                    try {
+                      await this.printFieldContext.init()
+                    } catch(e) {
+                      toastDanger(`${e}`)
+                    }
+                    this.isRefreshingDevices = false
                     loadingController.dismiss()
                   }
                 }
               }
             ]
+          },
+          init: () => {
+            this.refreshInterval = setInterval(async () => {
+              if (this.printFieldContext && !this.isRefreshingDevices) {
+                this.isRefreshingDevices = true
+                await this.printFieldContext.init()
+                this.isRefreshingDevices = false
+              }
+            }, 5000)
           },
           onload: (context: any) => {
             this.printFieldContext = context
@@ -104,20 +144,6 @@ export default defineComponent({
         }
       ]
     },
-  },
-  data() {
-    return {
-      printFieldContext: null as any,
-      printerService: {} as PrintoutService,
-      defaultPrinter: {} as Record<string, any>,
-      refreshKey: 0,
-      fields: [] as any,
-    };
-  },
-  async mounted() {
-    this.printerService = new PrintoutService();
-    this.defaultPrinter = await this.printerService.getDefaultPrinter();
-    this.fields = this.getFields();
   },
 });
 </script>
