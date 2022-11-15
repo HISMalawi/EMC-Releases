860df1b6872dbd79c7114d24bfbb74b0224e1468
Andrew Mfune <andiemfune@gmail.com>
2022-11-15 13:43:18 +0200

    Added aggregated rows in report

--- src/apps/ART/views/reports/pepfar/TxCurrMl.vue
+++ src/apps/ART/views/reports/pepfar/TxCurrMl.vue
@@ -24,6 +24,7 @@ import ReportTemplate from "@/apps/ART/views/reports/TableReportTemplate.vue"
 import table from "@/components/DataViews/tables/ReportDataTable"
 import { IonPage } from "@ionic/vue"
 import { AGE_GROUPS } from "@/apps/ART/services/reports/patient_report_service"
+import { uniq } from 'lodash'
 
 export default defineComponent({
     mixins: [ReportMixin],
@@ -44,6 +45,14 @@ export default defineComponent({
                 table.thTxt('Refused (Stopped)')
             ]
         ],
+        indexLabel:[
+            'Died',
+            'IIT <3 mo',
+            'IIT 3-5 mo',
+            'IIT 6+ mo',
+            'Tranferred out',
+            'Refused (Stopped)'
+        ],
         cohort: {} as any
     }),
     created() {
@@ -57,15 +66,17 @@ export default defineComponent({
             this.report.setEndDate(config.end_date)
             this.period = this.report.getDateIntervalPeriod()
             this.cohort = await this.report.getTxMlReport()
-            await this.setRows('F')
-            await this.setRows('M')
+            this.setRows('F')
+            this.setRows('M')
+            this.setTotalMaleRow()
+            this.setMaternalRows()
         },
         drilldown(patients: Array<number>, context: string) {
             const columns = [
                 [
-                    table.thTxt('ARV#'), 
-                    table.thTxt('DOB'), 
-                    table.thTxt('Dispensed'), 
+                    table.thTxt('ARV#'),
+                    table.thTxt('DOB'),
+                    table.thTxt('Dispensed'),
                     table.thTxt('ARVs')
                 ]
             ]
@@ -106,15 +117,83 @@ export default defineComponent({
             }
             return table.tdLink(patients.length, () => this.drilldownAsyncRows(context, columns, asyncRows))
         },
-        async setRows(gender: string) {
-            const contexts: any = [
-                'Defaulted (new registration)',
-                'Defaulted (old registration)',
-                'Died',
-                'Stopped',
-                'Tranferred out',
-                'Unknown'
-            ]
+        aggregate(gender: 'M' | 'F', indicator: string): Array<any> {
+            return Object.values(this.cohort).reduce((patients: any, c: any) => {
+                return c[gender] ? [...c[gender][this.indexLabel.indexOf(indicator)], ...patients] : patients
+            }, []) as Array<any>
+        },
+        setTotalMaleRow() {
+            const drill = (label: string) => this.drilldown(this.aggregate('M', label), label)
+            this.rows.push([
+                table.td('All'),
+                table.td('Male'),
+                drill('Died'),
+                drill('IIT <3 mo'),
+                drill('IIT 3-5 mo'),
+                drill('IIT 6+ mo'),
+                drill('Tranferred out'),
+                drill('Refused (Stopped)')
+            ])
+        },
+        async setMaternalRows() {
+            const indicators = this.indexLabel.reduce((aggregated: any, indicator: string) => [
+                ...aggregated, { indicator, data: this.aggregate('F', indicator)}
+            ], [])
+    
+            const maternalStatus = await this.report.getMaternalStatus(
+                uniq(indicators.reduce((totals: any, cur: any) => [...totals, ...cur.data], []).map((id: number) => id))
+            )
+
+            const groupBy = (indicator: string) => indicators.reduce(
+                (all: any, i: any) => i.indicator === indicator ? [...all, ...i.data] : all, []
+            )
+
+            const fP = (s: 'FP' | 'FBf', indicator: string) => {
+                return this.drilldown(
+                    groupBy(indicator).filter((patient: any) => maternalStatus[s].includes(patient)), `All ${indicator} Female Pregnant`
+                )
+            }
+
+            const allPregnant = maternalStatus.FBf.concat(maternalStatus.FP)
+
+            const fnP = (indicator: string) => {
+                return this.drilldown(
+                    groupBy(indicator).filter((patient: any) => !allPregnant.includes(patient)), `All ${indicator} Female not pregnant`
+                )
+            }
+
+            this.rows.push([
+                table.td('All'),
+                table.td('FP'),
+                fP('FP', 'Died'),
+                fP('FP', 'IIT <3 mo'),
+                fP('FP', 'IIT 3-5 mo'),
+                fP('FP', 'IIT 6+ mo'),
+                fP('FP', 'Tranferred out'),
+                fP('FP', 'Refused (Stopped)')
+            ])
+            this.rows.push([
+                table.td('All'),
+                table.td('FNP'),
+                fnP('Died'),
+                fnP('IIT <3 mo'),
+                fnP('IIT 3-5 mo'),
+                fnP('IIT 6+ mo'),
+                fnP('Tranferred out'),
+                fnP('Refused (Stopped)')
+            ])
+            this.rows.push([
+                table.td('All'),
+                table.td('FBf'),
+                fP('FBf', 'Died'),
+                fP('FBf', 'IIT <3 mo'),
+                fP('FBf', 'IIT 3-5 mo'),
+                fP('FBf', 'IIT 6+ mo'),
+                fP('FBf', 'Tranferred out'),
+                fP('FBf', 'Refused (Stopped)')
+            ])
+        },
+        setRows(gender: string) {
             for(const i in AGE_GROUPS) {
                 const group = AGE_GROUPS[i]
                 const fullGender = this.formatGender(gender);
@@ -122,7 +201,7 @@ export default defineComponent({
                     const cohortData = this.cohort[group][gender]
                     const drillContext = `${fullGender}s ${group}`
                     const drillable = cohortData.map(
-                        (d: Array<number>, i: number) => this.drilldown(d, `${drillContext} ${contexts[i]}`)
+                        (d: Array<number>, i: number) => this.drilldown(d, `${drillContext} ${this.indexLabel[i]}`)
                     )
                     this.rows.push([
                         table.td(group),
