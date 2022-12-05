ff40ab79da8e07931eac115427f5aeaecfde4eb5
Daniel Justin <danieljmuleke@gmail.com>
2022-12-05 15:29:38 +0200

    Initial sort report in descending order

--- src/apps/EMC/views/reports/moh/SurvivalAnalysis.vue
+++ src/apps/EMC/views/reports/moh/SurvivalAnalysis.vue
@@ -29,7 +29,7 @@ export default defineComponent({
     const ageGroup = reactive({} as Option)
     const rows = ref<any[]>([]);
     const columns: TableColumnInterface[] = [
-      { path: "quarter", label: "Reg Cohort", initialSort: true, initialSortOrder: 'asc' },
+      { path: "quarter", label: "Reg Cohort", initialSort: true, initialSortOrder: 'desc' },
       { path: "interval", label: "Interval (Months)" },
       { path: "subgroup", label: "Sub group" },
       { path: "totalReg", label: "Total Reg (database)" },
