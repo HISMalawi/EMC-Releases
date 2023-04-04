277258d415ce7a5891d38c94888dc6eda9af5263
Andre Mfune <andiemfune@gmail.com>
2023-04-04 15:04:42 +0200

    Custom USB plugin fix for printing (#679)
    -  Updated changes to align with guidelines for capacitor 4. See: https://capacitorjs.com/docs/updating/4-0
    
    Note: Issue was affecting Ocom PoC devices that use zebra printers via USB

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -7,9 +7,9 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
     Globals.setActivity(this);
     Globals.setContext(this.getApplicationContext());
     registerPlugin(LabelPrinterPlugin.class);
+    super.onCreate(savedInstanceState);
   }
 }
