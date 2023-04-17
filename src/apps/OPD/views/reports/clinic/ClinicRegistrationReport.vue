8a663bc91f02e2baba3763d30a5ac81d58a8b525
petroskayange <kayangepetros@gmail.com>
2023-04-17 10:21:40 +0200

    OPD registration report adding under 5 and over 5 year  Changes to be committed: 	modified:   src/apps/OPD/components/RegistrationSummaryModal.vue 	modified:   src/apps/OPD/views/reports/clinic/ClinicRegistrationReport.vue

--- src/apps/OPD/views/reports/clinic/ClinicRegistrationReport.vue
+++ src/apps/OPD/views/reports/clinic/ClinicRegistrationReport.vue
@@ -24,6 +24,7 @@ import ReportMixin from '@/apps/ART/views/reports/ReportMixin.vue'
 import { modalController } from '@ionic/core'
 import SummaryModal from "@/apps/OPD/components/RegistrationSummaryModal.vue";
 import { IonPage } from "@ionic/vue";
+import { Service } from "@/services/service";
 
 export default defineComponent({
   components: { ReportTemplate, IonPage },
@@ -77,11 +78,39 @@ export default defineComponent({
         table.td(HisDate.toStandardHisDisplayFormat(record.visit_date)),
       ])
     },
+    calculateAge(date1:any, date2:any) {
+      const birthDate = new Date(date1);
+      const currentDate = new Date(date2);
+      let ageDifference = currentDate.getFullYear() - birthDate.getFullYear();
+      const monthDifference = currentDate.getMonth() - birthDate.getMonth();
+      if (monthDifference < 0 || (monthDifference === 0 && currentDate.getDate() < birthDate.getDate())) {
+        ageDifference--;
+      }
+      return ageDifference;
+    },
+    filterVisitType(visitType:any){
+      return this.reportData.filter((d: any) => d.visit_type === visitType)
+    },
+    filterAge(data:any,ageType:any){
+      if(ageType == 'under5')
+        return data.filter((d: any) => this.calculateAge(d.birthdate,Service.getSessionDate()) <= 5)
+      else if(ageType == 'over5')
+        return data.filter((d: any) => this.calculateAge(d.birthdate,Service.getSessionDate()) > 5)
+    },
     async showModal() {
       const data = [
-        { label: "New patient", value: [...this.reportData.filter((d: any) => d.visit_type === 'New patient')].length },
-        { label: "Referral", value: [...this.reportData.filter((d: any) => d.visit_type === 'Referral')].length },
-        { label: "Revisiting", value: [...this.reportData.filter((d: any) => d.visit_type === 'Revisiting')].length }
+        { label:  "New patient",
+          under5:  [...this.filterAge(this.filterVisitType('New patient'),'under5')].length, 
+          over5:  [...this.filterAge(this.filterVisitType('New patient'),'over5')].length, 
+          value:  [...this.filterVisitType('New patient')].length },
+        { label:  "Referral", 
+          under5:  [...this.filterAge(this.filterVisitType('Referral'),'under5')].length, 
+          over5:  [...this.filterAge(this.filterVisitType('Referral'),'over5')].length, 
+          value:  [...this.filterVisitType('Referral')].length },
+        { label:  "Revisiting", 
+          under5:  [...this.filterAge(this.filterVisitType('Revisiting'),'under5')].length,
+          over5:  [...this.filterAge(this.filterVisitType('Revisiting'),'over5')].length, 
+          value:  [...this.filterVisitType('Revisiting')].length }
       ]
       const modal = await modalController.create({
         component: SummaryModal,
