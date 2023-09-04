af6229ddd0c293c1d5491ef2fb9dd0a3e90d9bde
anthony-kakatera <anthonyckakatera@gmail.com>
2023-09-04 15:51:05 +0200

    Added encounter to routes

--- src/apps/AETC/config/AetcRoutes.ts
+++ src/apps/AETC/config/AetcRoutes.ts
@@ -2,6 +2,11 @@ import { AETC_GLOBAL_PROP } from "../aetc_global_props"
 
 export default [
     {
+        name: "patient registration",
+        path: "/opd/encounters/registration/:patient_id",
+        component: () => import('@/apps/OPD/views/encounters/Registration.vue')
+    },
+    {
         name: AETC_GLOBAL_PROP.SET_TOP_10_DIAGNOSIS,
         path: "/aetc/preferences/top_ten_diagnosis",
         component: () => import("@/apps/AETC/views/preferences/DefaultPreferences.vue")
