6ecc7937e82090cd9359e1d95fc9df25b6ec135e
anthony-kakatera <anthonyckakatera@gmail.com>
2023-09-13 16:12:00 +0200

    Added methods to hit the endpoint

--- src/apps/AETC/services/aetc_report_service.ts
+++ src/apps/AETC/services/aetc_report_service.ts
@@ -23,4 +23,28 @@ export class OpdReportService extends Service {
         this.date = Service.getSessionDate()
         this.programID = Service.getProgramID()
     }
+
+    getClinicReport(reportName: string) {
+        return this.getReport('clinic_aetc', {'report_name': reportName})
+    }
+
+    getMOHReport(reportName: string) {
+        return this.getReport('moh_aetc', {'report_name': reportName})
+    }
+
+    getReport(url: string, params={}) {
+        return Service.getJson(url, this.buildRequest(params))
+    }
+
+    buildRequest(config: Record<string, any> = {}) {
+        const payload: any = {'date': this.date, 'program_id': this.programID}
+        if (this.startDate && this.endDate) {
+            payload['start_date'] = this.startDate
+            payload['end_date'] = this.endDate
+        }
+        if (this.epiweek) {
+            payload['epiweek'] = this.epiweek
+        }
+        return { ...payload, ...config }
+    }
 }
