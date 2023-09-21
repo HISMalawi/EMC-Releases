07944544fb0f27af51cfccd9782efefa06c4ca42
Daniel Justin <danieljmuleke@gmail.com>
2023-09-21 14:24:33 +0200

    Refactor: Improved code reuse

--- src/apps/EMC/utils/exports.ts
+++ src/apps/EMC/utils/exports.ts
@@ -17,43 +17,49 @@ export function sanitize(str: string) {
   }
 }
 
-interface CsvOptions {
+interface ExportOptions {
   columns: TableColumnInterface[];
   rows: any[];
   filename: string;
   quarter?: string;
   period?: string;
   filters?: TableFilterInterface;
+  encryption?: object;
+  canHorizontalPageBreak?: boolean;
 }
 
-function convertToCsv({columns, rows, quarter, period, filters}: CsvOptions) {
-  let str = columns.filter((column) => column.exportable !== false)
-    .map((column) => column.label)
-    .join(",");
+function getExportableHeadings(columns: Array<TableColumnInterface>): Array<string> {
+  return columns.filter(c => c.exportable !== false).map(c => c.label);
+}
 
-  str += "\n";
-  str += sortRows(rows, filters?.sort || []).map((row) => columns
-    .filter(column => column.exportable !== false)
-    .map(column => {
-      let value = get(row, column.path);
-      if (typeof column.formatter === 'function' && value) value = column.formatter(value, row)
-      return sanitize(column.drillable && Array.isArray(value) ? value.length : value);
-    })
-    .join(",")
-  ).join("\n");
+function getExportableRows(columns: Array<TableColumnInterface>, rows: Array<any>): Array<Array<string>> {
+  return rows.map(row => {
+    return columns.filter(column => column.exportable !== false)
+      .map(column => {
+        let value = get(row, column.path);
+        if (typeof column.formatter === 'function' && value) value = column.formatter(value, row)
+        return sanitize(column.drillable && Array.isArray(value) ? value.length : value);
+      })
+  })
+}
 
+function toCsvString(opts: ExportOptions) {
+  const {columns, rows, quarter, period, filters} = opts;
+  const exportableColumns = getExportableHeadings(columns);
+  const exportableRows = getExportableRows(columns, sortRows(rows, filters?.sort || []));
+  let str = exportableColumns.join(",") + "\n"
+  str += exportableRows.map(row => row.join(",")).join("\n");
   str += "\n" + `Date Created:  ${dayjs().format('DD/MMM/YYYY HH:MM:ss')}`;
   if (quarter) str += "\n" + `Quarter: ${quarter}`;
   if (period) str += "\n" + `Quarter: ${period}`;
   str += "\n" + `e-Mastercard Version : ${Service.getAppVersion()}`;
   str += "\n" + `API Version ${Service.getApiVersion()}`;
   str += "\n" + `Site UUID: ${Service.getSiteUUID()}`;
-
   return str;
 }
 
-export function exportToCSV(opts: CsvOptions) {
-  const csvData = new Blob([convertToCsv(opts)], { type: "text/csv;charset=utf-8;" });
+export function exportToCSV(opts: ExportOptions) {
+  const csvData = new Blob([toCsvString(opts)], { type: "text/csv;charset=utf-8;" });
   const link = document.createElement("a");
   link.href = window.URL.createObjectURL(csvData);
   link.setAttribute("download", `${opts.filename}.csv`);
