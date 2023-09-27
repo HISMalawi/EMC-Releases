699c1fc095e8822b109b0169741c0da9a8813788
anthony-kakatera <anthonyckakatera@gmail.com>
2023-09-27 15:19:20 +0200

    Changed names to aline with encounters so that when a task is complete the encounter cancels out

--- src/apps/AETC/config/programActivities.ts
+++ src/apps/AETC/config/programActivities.ts
@@ -3,34 +3,34 @@ import { TaskInterface } from "@/apps/interfaces/TaskInterface";
 export const PRIMARY_ACTIVITIES: TaskInterface[] = [
   {
     id: 'AETC patient registration',
-    name: 'AETC Patient registration',
+    name: 'Patient registration',
     icon: 'attributes.png'
   },
   {
     id: 'AETC vitals',
-    name: 'AETC vitals',
+    name: 'vitals',
     icon: 'vitals.png'
   },
   {
     id: 'AETC presenting complaints',
-    name: 'AETC presenting complaints',
+    name: 'presenting complaints',
     icon: 'complaints.png'
   },
   {
     id: 'AETC hiv status',
-    name: 'AETC hiv status',
+    name: 'hiv status',
     icon: 'aids.png',
     encounterTypeName: 'update hiv status',
     availableOnActivitySelection: false
   },
   {
     id: 'AETC outpatient diagnosis',
-    name: 'AETC outpatient diagnosis',
+    name: 'outpatient diagnosis',
     icon: 'diagnosis.png'
   },
   {
     id: 'AETC outcome status',
-    name: 'AETC outcome status',
+    name: 'outcome status',
     icon: 'outcomes.png',
     availableOnActivitySelection: false
   },
@@ -54,18 +54,18 @@ export const PRIMARY_ACTIVITIES: TaskInterface[] = [
   },
   {
     id: 'AETC social history',
-    name: 'AETC social history',
+    name: 'social history',
     icon: 'medical-report.png'
   },
   {
     id: 'AETC prescription',
-    name: 'AETC prescription',
+    name: 'prescription',
     encounterTypeName: 'Treatment',
     icon: 'drugs-given.png'
   },
   {
     id: 'AETC dispensation',
-    name: 'AETC dispensation',
+    name: 'dispensation',
     workflowID:"Dispensing",
     icon: 'dispensing.png',
     availableOnActivitySelection: false
@@ -75,7 +75,7 @@ export const PRIMARY_ACTIVITIES: TaskInterface[] = [
 export const SECONDARY_ACTIVITIES: TaskInterface[] = [
   {
     id: 'AETC radiology examination',
-    name: 'AETC radiology examination',
+    name: 'radiology examination',
     icon: 'radiology_types.png'
   },
 ]
