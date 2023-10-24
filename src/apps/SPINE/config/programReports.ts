e67081d6b1c96f3ac5b84b9fd1c29c72c93fd9bc
Daniel Justin <danieljmuleke@gmail.com>
2023-10-24 13:27:40 +0200

    Implemented clinic diagnosis report

--- src/apps/SPINE/config/programReports.ts
+++ src/apps/SPINE/config/programReports.ts
@@ -1,3 +1,15 @@
 import { FolderInterface } from "@/apps/interfaces/AppInterface"
 
-export const REPORTS: FolderInterface[] = []
+export const REPORTS: FolderInterface[] = [
+  {
+    name: 'Clinic',
+    icon: 'reports.png',
+    defaultFilesIcon: 'reports.png',
+    files: [
+        {
+            name: 'Diagnosis Report',
+            pathName: 'Clinic Diagnosis Report'
+        },
+    ]
+},
+]
