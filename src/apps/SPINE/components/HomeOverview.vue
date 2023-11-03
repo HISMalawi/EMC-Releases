35713a0964cd328b97fcf229d7bc0a1a55dcad07
Daniel Justin <danieljmuleke@gmail.com>
2023-11-03 13:58:02 +0200

    Integrated the dashboard with API

--- src/apps/SPINE/components/HomeOverview.vue
+++ src/apps/SPINE/components/HomeOverview.vue
@@ -43,34 +43,42 @@
 
 <script lang="ts" setup>
 import { IonGrid, IonRow, IonCol } from "@ionic/vue";
-import { ref } from "vue";
-import dayjs from "dayjs";
+import { computed, onMounted, ref } from "vue";
 import ApexChart from "vue3-apexcharts";
 import StatCard from '@/apps/SPINE/components/StatCard.vue'
+import { SpineReportService } from "../services/spine_report_service";
+import { isEmpty } from "lodash";
+import dayjs from "dayjs";
 
-const sessionDate = ref("");
-const tableRows = ref([
-  { encounter: "Patient Registration", female: 0, male: 0, me: 0, facility: 0 },
-  { encounter: "HIV tests", female: 0, male: 0, me: 0, facility: 0 },
-  { encounter: "Influeza data", female: 0, male: 0, me: 0, facility: 0},
-  { encounter: "Chronic conditions", female: 0, male: 0, me: 0, facility: 0 },
-  { encounter: "Patient Diagnosis", female: 0, male: 0, me: 0, facility: 0 },
-  { encounter: "Prescriptions", female: 0, male: 0, me: 0, facility: 0 },
-  { encounter: "Dispensations", female: 0, male: 0, me: 0, facility: 0 },
-  { encounter: "Discharges", female: 0, male: 0, me: 0, facility: 0 },
-]);
+const stats = ref<any>();
+const tableRows = computed(() => isEmpty(stats.value) 
+  ? [
+      { encounter: "Patient Registration", female: 0, male: 0, me: 0, facility: 0 },
+      { encounter: "HIV tests", female: 0, male: 0, me: 0, facility: 0 },
+      { encounter: "Influeza data", female: 0, male: 0, me: 0, facility: 0},
+      { encounter: "Chronic conditions", female: 0, male: 0, me: 0, facility: 0 },
+      { encounter: "Patient Diagnosis", female: 0, male: 0, me: 0, facility: 0 },
+      { encounter: "Prescriptions", female: 0, male: 0, me: 0, facility: 0 },
+      { encounter: "Dispensations", female: 0, male: 0, me: 0, facility: 0 },
+      { encounter: "Discharges", female: 0, male: 0, me: 0, facility: 0 },
+    ]
+  : stats.value.rows
+);
+ 
+const patientSummaryStats = computed(() => [ 
+    { label: 'Registered today', value: stats.value?.patient_summary_stats?.top?.registered_today ?? - 1, color: 'lightyellow' },
+    { label: 'Returning today', value: stats.value?.patient_summary_stats?.top?.returning_today ?? - 1, color: 'lightyellow' },
+    { label: 'Referred today', value: stats.value?.patient_summary_stats?.top?.referred_today ?? - 1, color: 'lightyellow' },
+    { label: 'Total', value: getTotalVisits(stats.value?.patient_summary_stats?.top ?? {}) ?? - 1, color: 'yellowgreen' },
+  ]
+);
 
-    
-const patientSummaryStats = ref([
-  { label: 'Registered today', value: -1, color: 'lightyellow' },
-  { label: 'Returning today', value: -1, color: 'lightyellow' },
-  { label: 'Referred today', value: -1, color: 'lightyellow' },
-  { label: 'Total', value: -1, color: 'yellowgreen' },
-]);
+function getTotalVisits (data: any) {
+  return Object.values(data).reduce((acc: number, curr: any) => acc += curr, 0)
+}
 
 const seriesVisits = ref<Array<any>>([]);
-
-const options = {
+const options = ref({
   chart: {
     id: "vuechart-example",
   },
@@ -96,7 +104,28 @@ const options = {
       fontFamily: "Helvetica"
     }  
   }
+});
+
+function buildGraphData (chartData: any ) {
+  Object.entries(chartData).forEach(([name, values]: any) => {
+    const startDates = [] as Array<any>; 
+    const data = [] as Array<any>;
+    Object.values(values).forEach(({start_date, count}: any) => {
+      startDates.push(dayjs(start_date).format("MMM/YYYY"));
+      data.push(count);
+    });
+    seriesVisits.value.push({ data, name });
+    options.value.xaxis.categories = startDates
+  })
 }
+
+onMounted(async () => {
+  const service = new SpineReportService();
+  service.startDate = service.date;
+  service.endDate = service.date;
+  stats.value = await service.getReport("dashboard");
+  buildGraphData(stats.value.patient_summary_stats.down);
+})
 </script>
 
 <style scoped>
