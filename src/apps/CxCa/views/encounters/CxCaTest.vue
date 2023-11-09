66d7b22ff332cac39f0987b70bec307c817ac104
anthony-kakatera <anthonyckakatera@gmail.com>
2023-11-09 10:39:01 +0200

    refactored code and removed console logs

--- src/apps/CxCa/views/encounters/CxCaTest.vue
+++ src/apps/CxCa/views/encounters/CxCaTest.vue
@@ -19,7 +19,7 @@ import { generateDateFields, EstimationFieldType } from "@/utils/HisFormHelpers/
 import { getFacilities } from "@/utils/HisFormHelpers/LocationFieldOptions";
 import { ConceptService } from "@/services/concept_service";
 import { ProgramService } from "@/services/program_service";
-import { ObservationService } from '@/services/observation_service';
+import { PatientProgramService } from "@/services/patient_program_service";
 
 export default defineComponent({
   mixins: [EncounterMixinVue],
@@ -41,17 +41,15 @@ export default defineComponent({
           this.providerID
         );
         // Retrieving previous treatment if available
-        this.patientPreviousTreatment = await ObservationService.getFirstValueCoded(this.patientID, 'Treatment')
+        this.patientPreviousTreatment = await this.assessment.getFirstValueCoded('Treatment')
         // Retrieving HIV Status if available
-        this.patientPreviousVisitHIVStatus = await ObservationService.getFirstValueCoded(this.patientID, 'HIV status')
+        this.patientPreviousVisitHIVStatus = await this.assessment.getFirstValueCoded('HIV status')
 
         //test here 
         const program = await ProgramService.getProgramInformation(this.patientID)
 
-        // patient already enrolled
-        this.alreadyEnrolled = this.patientAlreadyEnrolled()
-
-        console.log("Patient already enrolled >>> ", this.alreadyEnrolled)
+        // assigns boolean value to alreadyEnrolled
+        await this.patientAlreadyEnrolled()
 
         ConceptService.getConceptsByCategory("reason_for_no_cxca")
 
@@ -82,10 +80,16 @@ export default defineComponent({
       const medicalProcedures = ["Positive on ART", "Positive Not on ART"];
       return medicalProcedures.includes(this.patientPreviousVisitHIVStatus.toLowerCase());
     },
-    
-    patientAlreadyEnrolled(){
-      return !(this.isNullOrUndefined(this.patientPreviousTreatment))
+
+    async patientAlreadyEnrolled(){
+      const program_service = new PatientProgramService(this.patientID)
+      const programs = await program_service.getPrograms();
+      this.alreadyEnrolled = programs.some((p: any) => /CxCa program/i.test(p.program.name));
     },
+    
+    // patientAlreadyEnrolled(){
+    //   return !(this.isNullOrUndefined(this.patientPreviousTreatment))
+    // },
     isNullOrUndefined(str: string | null | undefined): boolean {
       return typeof str === 'undefined' || str === null;
     },
