0e3ed2f609ef4931db765c5131c8acb4cfa9a8e7
Daniel Justin <danieljmuleke@gmail.com>
2023-11-21 10:09:40 +0200

    ANC prenatal visit number validation (#870)
    * Auto increment visit number
    
    * Capture reason for having more than 8 visits
    
    * Fix condition for capturing reasons
    
    * Reduced the max acceptable number of visits to 15

--- src/apps/ANC/Views/Encounters/VisitType.vue
+++ src/apps/ANC/Views/Encounters/VisitType.vue
@@ -13,7 +13,7 @@
 import EncounterMixinVue from '@/views/EncounterMixin.vue'
 import { FieldType } from "@/components/Forms/BaseFormElements"
 import { defineComponent } from 'vue'
-import { Option } from '@/components/Forms/FieldInterface'
+import { Field, Option } from '@/components/Forms/FieldInterface'
 import Validation from "@/components/Forms/validations/StandardValidations"
 import { IonPage } from "@ionic/vue"
 import { AncVisitTypeService } from "@/apps/ANC/Services/anc_visit_type_service"
@@ -22,7 +22,7 @@ export default defineComponent({
   components: { IonPage },
   mixins: [EncounterMixinVue],
   data: () => ({
-    service: {} as any,
+    service: {} as AncVisitTypeService,
     helpText: 'ANC Visit Number' as string
   }),
   watch: {
@@ -39,12 +39,12 @@ export default defineComponent({
   },
   methods: {
     async onFinish(_: any, c: any) {
-      const visit = new AncVisitTypeService(this.patientID, this.providerID)
-      await visit.createEncounter()
-      await visit.saveVisitNumber(c['visit_number'])
+      await this.service.createEncounter();
+      const obs = await this.resolveObs(c);
+      await this.service.saveObservationList(obs);
       this.nextTask()
     },
-    getFields() {
+    getFields(): Array<Field> {
       return [
         {
           id: 'visit_number',
@@ -55,12 +55,26 @@ export default defineComponent({
             }
             return this.helpText
           },
+          defaultValue: () => this.service.lastVisitNumber + 1,
           type: FieldType.TT_NUMBER,
-          computedValue: (v: Option) => v.value,
+          computedValue: (v: Option) => ({ 
+            obs: [
+              this.service.buildValueNumber("Type of visit", v.value as number),
+              this.service.buildValueCoded("Has extended ANC visits", v.value as number > 8 ? "Yes" : "No")
+            ]
+          }),
           validation: (v: Option) => this.validateSeries([
             () => Validation.required(v),
-            () => Validation.rangeOf(v, this.service.lastVisitNumber+1, 40)
+            () => Validation.rangeOf(v, this.service.lastVisitNumber + 1, 15)
           ])
+        },
+        {
+          id: 'reasons',
+          helpText: "Provide reason for having more than 8 visits",
+          type: FieldType.TT_TEXT,
+          computedValue: (v: Option) => ({ obs: this.service.buildValueText("Reason for extended ANC visits", v.value as string) }),
+          condition: (f: any) => f.visit_number.value > 8,
+          validation: (v: Option) =>  Validation.required(v),
         }
       ]
     }
